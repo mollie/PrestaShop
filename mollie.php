ef9b6d5744b07b1ddcc5d8bb3f59e15e53360f0e
Snowy Cat Software <info@snowycatsoftware.com>
2018-03-21 15:05:02 +0100

    Bump to v2.0.2

--- mollie.php
+++ mollie.php
@@ -133,7 +133,7 @@ class Mollie extends PaymentModule
     {
         $this->name = 'mollie';
         $this->tab = 'payments_gateways';
-        $this->version = '2.0.1';
+        $this->version = '2.0.2';
         $this->author = 'Mollie B.V.';
         $this->need_instance = 1;
         $this->bootstrap = true;
