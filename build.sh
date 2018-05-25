4fc74ae2a0cb4500b3930cf5b27588525202ad55
Snowy Cat Software <info@snowycatsoftware.com>
2018-05-25 02:33:49 +0200

    Optimize build script

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ if [ ! -d "${CWD_BASEDIR}/lib/vendor/" ]; then
   composer install --no-dev --prefer-dist
   composer -o dump-autoload
   find vendor/ -type d -exec cp index.php {} \;
-  rm rm ${CWD_BASEDIR}/lib/vendor/mollie/mollie-api-php/examples -rf
+  rm ${CWD_BASEDIR}/lib/vendor/firstred/mollie-api-php/examples -rf
   cd ..
 fi
 
@@ -19,6 +19,10 @@ rm -rf ${CWD_BASEDIR}/views/js/app/dist/
 NODE_ENV=production webpack --mode production
 cp ${CWD_BASEDIR}/views/js/app/index.php ${CWD_BASEDIR}/views/js/app/dist/index.php
 cd ${CWD_BASEDIR}
+
+${CWD_BASEDIR}/lib/vendor/firstred/mollie-api-php/.git -rf
+${CWD_BASEDIR}/lib/vendor/firstred/mollie-api-php/tests -rf
+${CWD_BASEDIR}/lib/vendor/firstred/mollie-api-php/build -rf
 
 FILES=("logo.gif")
 FILES+=("logo.png")
@@ -46,6 +50,7 @@ ZIP_FILE="${CWD_BASENAME}/${CWD_BASENAME}-v${MODULE_VERSION}.zip"
 
 echo "Going to zip ${CWD_BASENAME} version ${MODULE_VERSION}"
 
+rm ${ZIP_FILE}
 cd ..
 for E in "${FILES[@]}"; do
   find ${CWD_BASENAME}/${E}  -type f -exec zip -9 ${ZIP_FILE} {} \;
