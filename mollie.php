d718b3951ffb219757fbc89285105f2f162950da
Michael Dekker <git@michaeldekker.nl>
2019-01-27 19:02:07 +0100

    Bump to v3.3.6

--- mollie.php
+++ mollie.php
@@ -252,7 +252,7 @@ class Mollie extends PaymentModule
     {
         $this->name = 'mollie';
         $this->tab = 'payments_gateways';
-        $this->version = '3.3.5';
+        $this->version = '3.3.6';
         $this->author = 'Mollie B.V.';
         $this->need_instance = 1;
         $this->bootstrap = true;
