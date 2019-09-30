8967009d91702fcf7d7a98304d0ddfca783e2b96
marius <marius.gudauskis@invertus.eu>
2019-09-30 14:16:05 +0300

    changed module version and added licenses

--- mollie.php
+++ mollie.php
@@ -267,7 +267,7 @@ class Mollie extends PaymentModule
     {
         $this->name = 'mollie';
         $this->tab = 'payments_gateways';
-        $this->version = '3.4.3';
+        $this->version = '3.4.4';
         $this->author = 'Mollie B.V.';
         $this->need_instance = 1;
         $this->bootstrap = true;
