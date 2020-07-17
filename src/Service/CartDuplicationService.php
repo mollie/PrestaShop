a6f6554743167c73d3b50d7f4f668a96a31592a5
Tomas Ilginis <tilginis@gmail.com>
2020-07-17 14:33:39 +0300

    removes cart voucher duplication

--- src/Service/CartDuplicationService.php
+++ src/Service/CartDuplicationService.php
@@ -59,10 +59,7 @@ class CartDuplicationService
         if ($duplication['success']) {
             /** @var Cart $duplicatedCart */
             $duplicatedCart = $duplication['cart'];
-            foreach ($cart->getOrderedCartRulesIds() as $cartRuleId) {
-                $duplicatedCart->addCartRule($cartRuleId['id_cart_rule']);
-                $this->restoreCartRuleQuantity($cartId, $cartRuleId['id_cart_rule']);
-            }
+
             $context->cookie->id_cart = $duplicatedCart->id;
             $context->cart = $duplicatedCart;
             CartRule::autoAddToCart($context);
@@ -72,16 +69,5 @@ class CartDuplicationService
         }
 
         return 0;
-    }
-
-    private function restoreCartRuleQuantity($cartId, $cartRuleId)
-    {
-        $cartRule = new CartRule($cartRuleId);
-        $cartRule->quantity++;
-        $cartRule->update();
-
-        $orderId = Order::getIdByCartId($cartId);
-        $sql = 'DELETE FROM `ps_order_cart_rule` WHERE id_order = ' . (int) $orderId . ' AND id_cart_rule = ' . (int) $cartRuleId;
-        DB::getInstance()->execute($sql);
     }
 }
