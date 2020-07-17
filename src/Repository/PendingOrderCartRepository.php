1e6a718d44e32d7bc90474f0b63d8e88662ac82f
Tomas Ilginis <tilginis@gmail.com>
2020-07-17 14:19:11 +0300

    removes pending order cancellation logic

--- src/Repository/PendingOrderCartRepository.php
+++ src/Repository/PendingOrderCartRepository.php
@@ -4,16 +4,4 @@ namespace Mollie\Repository;
 
 final class PendingOrderCartRepository extends AbstractRepository
 {
-    /**
-     * @param int $prestaCartId
-     * @return bool
-     * @throws \PrestaShopException
-     */
-    public function hasPendingCancellableOrder($prestaCartId)
-    {
-        return null !== $this->findOneBy([
-            'cart_id' => (int) $prestaCartId,
-            'should_cancel_order' => 1,
-        ]);
-    }
 }
