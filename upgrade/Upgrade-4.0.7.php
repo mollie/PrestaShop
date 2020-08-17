30244f047ba53d20beacc13ed5fdf0e9249e41df
jeroen <info@adeko.nl>
2020-08-17 10:33:58 +0200

    Fix Upgrade-4.0.7.php: Type error: Too few arguments to function Mollie\Install\Installer::__construct()

--- upgrade/Upgrade-4.0.7.php
+++ upgrade/Upgrade-4.0.7.php
@@ -72,7 +72,8 @@ function upgrade_module_4_0_7($module)
     $module->registerHook('actionAdminControllerSetMedia');
     $module->registerHook('actionValidateOrder');
 
-    $installer = new \Mollie\Install\Installer($module);
+    /** @var \Mollie\Install\Installer $installer */
+    $installer = $module->getContainer(\Mollie\Install\Installer::class);
     $installed = true;
 
     $installed &= $installer->installTab('AdminMollieAjax', 0, 'AdminMollieAjax', false);
