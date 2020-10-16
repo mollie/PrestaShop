2cdea6adc923bafd21447062a72c29e2e165fb6a
Tomas <tilginis@gmail.com>
2020-10-16 15:18:22 +0300

    removes timeout functionality (#92)

--- views/js/front/mollie_payment_method_click_lock_16.js
+++ views/js/front/mollie_payment_method_click_lock_16.js
@@ -1,20 +1,21 @@
 // the code in this file is related with preventing users from double clicking the payment method on 1.6 prestashop version.
 $(document).ready(function () {
-  // a helper to release the lock in case of an error.
-  var releaseLock = function($paymentMethod) {
-    setTimeout(function () { $paymentMethod.removeClass('locked') }, 2000)
-  }
+  var actionPlaceholder = '#'
 
   $(document).on('click', '.mollie_method', function (e) {
+    var href = $(this).attr('href')
+
+    // not disabling anything since its not a link action
+    if (href === actionPlaceholder) {
+      return;
+    }
+
     var $self = $(this);
 
     if ($self.hasClass('locked')) {
       e.preventDefault();
-      releaseLock($self)
     }
 
     $self.addClass('locked')
-
-    releaseLock($self)
   })
 })
