6ec3d9e8aaba3a6dfca09484795b1eff8a70a14e
Tomas Jasulaitis <tomas.jasulaitis@invertus.eu>
2020-12-10 10:50:46 +0200

    Fixed phone number provider to handle 0000 cases

--- tests/Unit/Provider/PhoneNumberProviderTest.php
+++ tests/Unit/Provider/PhoneNumberProviderTest.php
@@ -54,6 +54,14 @@
 //                'phoneNumber' => '+0164742671',
 //                'result' => '+164742671',
 //            ],
+//            'number that consists of 0s with +' => [
+//                'phoneNumber' => '+000000',
+//                'result' => null,
+//            ],
+//            'number that consists of 0s' => [
+//                'phoneNumber' => '000000',
+//                'result' => null,
+//            ],
 //        ];
 //    }
 //}
