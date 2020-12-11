a8cfb1394dd9e8e26c75fb327aaa0103f6f2b383
margud <marius.gudauskis@invertus.eu>
2020-12-11 16:13:15 +0200

    changed csv generation logic to add other languages to the translations

--- src/Command/TranslationCsvFileGeneratorConsoleCommand.php
+++ src/Command/TranslationCsvFileGeneratorConsoleCommand.php
@@ -43,52 +43,71 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class TranslationCsvFileGeneratorConsoleCommand extends Command
 {
-	/**
-	 * @var Mollie
-	 */
-	private $module;
+    /**
+     * @var Mollie
+     */
+    private $module;
 
-	public function __construct(Mollie $module)
-	{
-		parent::__construct();
-		$this->module = $module;
-	}
+    public function __construct(Mollie $module)
+    {
+        parent::__construct();
+        $this->module = $module;
+    }
 
-	protected function configure()
-	{
-		$this
-			->setName('mollie:generate-translation-csv')
-			->setAliases(['m:g:t:c'])
-			->setDescription('Generate translation csv');
-	}
+    protected function configure()
+    {
+        $this
+            ->setName('mollie:generate-translation-csv')
+            ->setAliases(['m:g:t:c'])
+            ->setDescription('Generate translation csv');
+    }
 
-	protected function execute(InputInterface $input, OutputInterface $output)
-	{
-		include_once $this->module->getLocalPath() . '/translations/en.php';
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        include_once $this->module->getLocalPath() . '/translations/en.php';
 
-		$csvHeader = [
-			'ID',
-			'EN',
-			'NL',
-			'DE',
-			'FR',
-		];
-		$translations = $GLOBALS['_MODULE'];
-		try {
-			$fp = fopen('translation.csv', 'w');
-			fputcsv($fp, $csvHeader);
-			foreach ($translations as $id => $text) {
-				$field = array_map('utf8_decode', [$id, $text]);
-				fputcsv($fp, $field);
-			}
-			fclose($fp);
-		} catch (\Exception $e) {
-			$output->writeln("<error>{$e->getMessage()}</error>");
+        $csvHeader = [
+            'ID',
+            'EN',
+            'NL',
+            'DE',
+            'FR',
+        ];
+        $translations = $GLOBALS['_MODULE'];
+        try {
+            $fp = fopen('translation.csv', 'w');
+            $fields = [];
+            foreach ($translations as $id => $text) {
+                $field = array_map('utf8_decode', [$id, $text]);
+                $fields[$field[0]] = $field;
+            }
+        } catch (\Exception $e) {
+            $output->writeln("<error>{$e->getMessage()}</error>");
 
-			return 0;
-		}
-		$output->writeln('<info>Translation export to CSV finished</info>');
+            return 0;
+        }
 
-		return 0;
-	}
+        $translationFiles = [
+            2 => 'nl',
+            3 => 'de',
+            4 => 'fr'
+        ];
+        fputcsv($fp, $csvHeader);
+        foreach ($translationFiles as $position => $file) {
+            include_once $this->module->getLocalPath() . "translations/{$file}.php";
+            $translations = $GLOBALS['_MODULE'];
+
+            foreach ($translations as $id => $text) {
+                $fields[$id][$position] = $text;
+            }
+        }
+        foreach ($fields as $field) {
+            fputcsv($fp, $field);
+        }
+
+        fclose($fp);
+        $output->writeln('<info>Translation export to CSV finished</info>');
+
+        return 0;
+    }
 }
