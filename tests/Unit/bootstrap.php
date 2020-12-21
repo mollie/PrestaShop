150ecc1fa65d4df3d25d025fe6669077f424bcb4
tomas.jasulaitis@invertus.eu <tomas.jasulaitis@invertus.eu>
2020-12-21 12:57:04 +0200

    testing workflow

--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -3,5 +3,5 @@
 $projectDir = __DIR__ . '/../../';
 require_once $projectDir . 'vendor/autoload.php';
 
-require_once $projectDir . '../../config/defines.inc.php';
+require_once $projectDir . 'config/defines.inc.php';
 require_once _PS_CONFIG_DIR_ . 'config.inc.php';
