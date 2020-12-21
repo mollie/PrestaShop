ceb5d6d8029af2f45876d7a72c3d6b1c6f407fd6
tomas.jasulaitis@invertus.eu <tomas.jasulaitis@invertus.eu>
2020-12-21 13:13:03 +0200

    Fixing bootstrap.php

--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -2,6 +2,3 @@
 
 $projectDir = __DIR__ . '/../../';
 require_once $projectDir . 'vendor/autoload.php';
-
-require_once $projectDir . 'config/defines.inc.php';
-require_once _PS_CONFIG_DIR_ . 'config.inc.php';
