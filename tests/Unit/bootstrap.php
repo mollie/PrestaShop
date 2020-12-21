1fa3cbea198336aa350f0a191880d9c33f299a6e
tomas.jasulaitis@invertus.eu <tomas.jasulaitis@invertus.eu>
2020-12-21 18:22:44 +0200

    Fixed phpunit tests

--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -4,4 +4,5 @@ $projectDir = __DIR__ . '/../../';
 require_once $projectDir . 'vendor/autoload.php';
 
 $rootDirectory = __DIR__ . '/../../../../';
-require_once $rootDirectory . 'config/config.inc.php';
+require_once $rootDirectory . 'autoload.php';
+require_once $rootDirectory . 'vendor/autoload.php';
