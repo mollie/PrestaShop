aa561415d2bf148f5134f717bfa39bcb78bc4093
tomjas1997 <tomjas1997@users.noreply.github.com>
2020-12-22 10:45:44 +0000

    Apply php-cs-fixer changes

--- tests/Unit/Handler/Shipment/ShipmentSenderHandlerTest.php
+++ tests/Unit/Handler/Shipment/ShipmentSenderHandlerTest.php
@@ -102,21 +102,21 @@ class ShipmentSenderHandlerTest extends TestCase
 			->willReturn(true)
 		;
 
-        $this->exceptionService
-            ->expects($this->never())
-            ->method('getErrorMessages')
-            ->willReturn([])
-        ;
+		$this->exceptionService
+			->expects($this->never())
+			->method('getErrorMessages')
+			->willReturn([])
+		;
 
-        $this->exceptionService
-            ->expects($this->never())
-            ->method('getErrorMessageForException')
-        ;
+		$this->exceptionService
+			->expects($this->never())
+			->method('getErrorMessageForException')
+		;
 
-        $this->moduleLogger
-            ->expects($this->never())
-            ->method('logException')
-        ;
+		$this->moduleLogger
+			->expects($this->never())
+			->method('logException')
+		;
 
 		$shipmentSenderHandler = new ShipmentSenderHandler(
 			$this->canSendShipment,
