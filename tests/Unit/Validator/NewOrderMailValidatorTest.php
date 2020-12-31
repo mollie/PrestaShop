ab99d7fe7446ab201f9dd3588f27f2df9d8e985d
tomjas1997 <tomjas1997@users.noreply.github.com>
2020-12-31 14:04:29 +0000

    Apply php-cs-fixer changes

--- tests/Unit/Validator/NewOrderMailValidatorTest.php
+++ tests/Unit/Validator/NewOrderMailValidatorTest.php
@@ -8,62 +8,62 @@ use PHPUnit\Framework\MockObject\MockObject;
 
 class NewOrderMailValidatorTest extends UnitTestCase
 {
-    /**
-     * @var ConfigurationAdapter|MockObject
-     */
-    private $configurationAdapter;
+	/**
+	 * @var ConfigurationAdapter|MockObject
+	 */
+	private $configurationAdapter;
 
-    protected function setUp(): void
-    {
-        parent::setUp();
+	protected function setUp(): void
+	{
+		parent::setUp();
 
-        $this->configurationAdapter = $this
-            ->getMockBuilder(ConfigurationAdapter::class)
-            ->disableOriginalConstructor()
-            ->getMock()
-        ;
-    }
+		$this->configurationAdapter = $this
+			->getMockBuilder(ConfigurationAdapter::class)
+			->disableOriginalConstructor()
+			->getMock()
+		;
+	}
 
-    /** @dataProvider getCanNewOrderMailBeSentData */
-    public function testCanNewOrderMailBeSent($orderStateId, $sendNewOrder, $paidOrderState, $outOfStockOrderState, $expected)
-    {
-        $this->configurationAdapter
-            ->expects($this->any())
-            ->method('get')
-            ->with(Config::MOLLIE_SEND_NEW_ORDER)
-            ->willReturn($sendNewOrder)
-        ;
+	/** @dataProvider getCanNewOrderMailBeSentData */
+	public function testCanNewOrderMailBeSent($orderStateId, $sendNewOrder, $paidOrderState, $outOfStockOrderState, $expected)
+	{
+		$this->configurationAdapter
+			->expects($this->any())
+			->method('get')
+			->with(Config::MOLLIE_SEND_NEW_ORDER)
+			->willReturn($sendNewOrder)
+		;
 
-        $this->configurationAdapter
-            ->expects($this->any())
-            ->method('get')
-            ->with(Config::MOLLIE_STATUS_PAID)
-            ->willReturn($paidOrderState)
-        ;
+		$this->configurationAdapter
+			->expects($this->any())
+			->method('get')
+			->with(Config::MOLLIE_STATUS_PAID)
+			->willReturn($paidOrderState)
+		;
 
-        $this->configurationAdapter
-            ->expects($this->any())
-            ->method('get')
-            ->with(Config::STATUS_PS_OS_OUTOFSTOCK_PAID)
-            ->willReturn($outOfStockOrderState)
-        ;
+		$this->configurationAdapter
+			->expects($this->any())
+			->method('get')
+			->with(Config::STATUS_PS_OS_OUTOFSTOCK_PAID)
+			->willReturn($outOfStockOrderState)
+		;
 
-        $newOrderMailValidator = new NewOrderMailValidator($this->configurationAdapter);
-        $result = $newOrderMailValidator->validate($orderStateId);
+		$newOrderMailValidator = new NewOrderMailValidator($this->configurationAdapter);
+		$result = $newOrderMailValidator->validate($orderStateId);
 
-        $this->assertEquals($expected, $result);
-    }
+		$this->assertEquals($expected, $result);
+	}
 
-    public function getCanNewOrderMailBeSentData()
-    {
-        return [
-            'Send on creation' => [
-                'orderStateId' => 'any',
-                'sendNewOrder' => Config::NEW_ORDER_MAIL_SEND_ON_CREATION,
-                'paidOrderState' => 55,
-                'outOfStockOrderState' => 60,
-                'expected' => true,
-            ],
-        ];
-    }
+	public function getCanNewOrderMailBeSentData()
+	{
+		return [
+			'Send on creation' => [
+				'orderStateId' => 'any',
+				'sendNewOrder' => Config::NEW_ORDER_MAIL_SEND_ON_CREATION,
+				'paidOrderState' => 55,
+				'outOfStockOrderState' => 60,
+				'expected' => true,
+			],
+		];
+	}
 }
