c0bb51eda75abd1b6f20b1c43b2decb2ba26ee83
margud <margud@users.noreply.github.com>
2021-01-08 08:56:25 +0000

    Apply php-cs-fixer changes

--- src/Command/UpdateTranslationsConsoleCommand.php
+++ src/Command/UpdateTranslationsConsoleCommand.php
@@ -20,64 +20,64 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class UpdateTranslationsConsoleCommand extends Command
 {
-    /**
-     * @var Mollie
-     */
-    private $module;
+	/**
+	 * @var Mollie
+	 */
+	private $module;
 
-    public function __construct(Mollie $module)
-    {
-        parent::__construct();
-        $this->module = $module;
-    }
+	public function __construct(Mollie $module)
+	{
+		parent::__construct();
+		$this->module = $module;
+	}
 
-    protected function configure()
-    {
-        $this
-            ->setName('mollie:update-translations')
-            ->setAliases(['m:u:t'])
-            ->setDescription('Update translation csv');
-    }
+	protected function configure()
+	{
+		$this
+			->setName('mollie:update-translations')
+			->setAliases(['m:u:t'])
+			->setDescription('Update translation csv');
+	}
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $missingTranslations = fopen('missing-translations.csv', 'r');
-        $data = fgetcsv($missingTranslations);
-        $missingTranslationsArray = [];
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$missingTranslations = fopen('missing-translations.csv', 'r');
+		$data = fgetcsv($missingTranslations);
+		$missingTranslationsArray = [];
 
-        while (($missingTranslation = fgetcsv($missingTranslations)) !== false) {
-            $missingTranslationsArray[$missingTranslation[0]] = $missingTranslation;
-        }
+		while (($missingTranslation = fgetcsv($missingTranslations)) !== false) {
+			$missingTranslationsArray[$missingTranslation[0]] = $missingTranslation;
+		}
 
-        try {
-            $translations = fopen('translation.csv', 'r');
-            $translationsArray = [];
-            while (($translation = fgetcsv($translations)) !== false) {
-                $translationsArray[$translation[0]] = $translation;
-            }
-        } catch (\Exception $e) {
-            $output->writeln("<error>{$e->getMessage()}</error>");
+		try {
+			$translations = fopen('translation.csv', 'r');
+			$translationsArray = [];
+			while (($translation = fgetcsv($translations)) !== false) {
+				$translationsArray[$translation[0]] = $translation;
+			}
+		} catch (\Exception $e) {
+			$output->writeln("<error>{$e->getMessage()}</error>");
 
-            return 0;
-        }
+			return 0;
+		}
 
-        foreach ($missingTranslationsArray as $position => $value) {
-            foreach ($translationsArray as $key => $item) {
-                if ($item[1] === $position) {
-                    $translationsArray[$key][2] = $value[1];
-                    $translationsArray[$key][3] = $value[2];
-                    $translationsArray[$key][4] = $value[3];
-                }
-            }
-        }
+		foreach ($missingTranslationsArray as $position => $value) {
+			foreach ($translationsArray as $key => $item) {
+				if ($item[1] === $position) {
+					$translationsArray[$key][2] = $value[1];
+					$translationsArray[$key][3] = $value[2];
+					$translationsArray[$key][4] = $value[3];
+				}
+			}
+		}
 
-        $translations = fopen('translation.csv', 'w');
-        foreach ($translationsArray as $value) {
-            fputcsv($translations, $value);
-        }
-        fclose($translations);
-        $output->writeln('<info>Translation export to CSV finished</info>');
+		$translations = fopen('translation.csv', 'w');
+		foreach ($translationsArray as $value) {
+			fputcsv($translations, $value);
+		}
+		fclose($translations);
+		$output->writeln('<info>Translation export to CSV finished</info>');
 
-        return 0;
-    }
+		return 0;
+	}
 }
