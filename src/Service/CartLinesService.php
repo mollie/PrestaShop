94664c7d964c2a3fce1549899f4f1665d8e29e75
margud <marius.gudauskis@invertus.eu>
2021-03-04 12:45:45 +0200

    fixed issue with order line rounding and refactored code so its better to understand

--- src/Service/CartLinesService.php
+++ src/Service/CartLinesService.php
@@ -87,8 +87,82 @@ class CartLinesService
 			$apiRoundingPrecision
 		);
 
-		$aItems = [];
+		$orderLines = [];
 		/* Item */
+		list($orderLines, $remaining) = $this->createProductLines($cartItems, $apiRoundingPrecision, $cartSummary['gift_products'], $orderLines, $selectedVoucherCategory, $remaining);
+
+		// Add discount if applicable
+		list($orderLines, $remaining) = $this->addDiscountsToProductLines($totalDiscounts, $apiRoundingPrecision, $orderLines, $remaining);
+
+		// Compensate for order total rounding inaccuracies
+		$orderLines = $this->compositeRoundingInaccuracies($remaining, $apiRoundingPrecision, $orderLines);
+
+		// Fill the order lines with the rest of the data (tax, total amount, etc.)
+		$orderLines = $this->fillProductLinesWithRemainingData($orderLines, $apiRoundingPrecision, $vatRatePrecision);
+
+		// Add shipping
+		$orderLines = $this->addShippingLine($shipping, $cartSummary, $apiRoundingPrecision, $orderLines);
+
+		// Add wrapping
+		$orderLines = $this->addWrappingLine($wrapping, $cartSummary, $vatRatePrecision, $apiRoundingPrecision, $orderLines);
+
+		// Add fee
+		$orderLines = $this->addPaymentFeeLine($paymentFee, $apiRoundingPrecision, $orderLines);
+
+		// Ungroup all the cart lines, just one level
+		$newItems = $this->ungroupLines($orderLines);
+
+		// Convert floats to strings for the Mollie API and add additional info
+		return $this->convertToLineArray($newItems, $currencyIsoCode, $apiRoundingPrecision);
+	}
+
+	/**
+	 * Spread the cart line amount evenly.
+	 *
+	 * Optionally split into multiple lines in case of rounding inaccuracies
+	 *
+	 * @param array[] $cartLineGroup Cart Line Group WITHOUT VAT details (except target VAT rate)
+	 * @param float $newTotal
+	 *
+	 * @return array[]
+	 *
+	 * @since 3.2.2
+	 * @since 3.3.3 Omits VAT details
+	 */
+	public static function spreadCartLineGroup($cartLineGroup, $newTotal)
+	{
+		$apiRoundingPrecision = Config::API_ROUNDING_PRECISION;
+		$newTotal = round($newTotal, $apiRoundingPrecision);
+		$quantity = array_sum(array_column($cartLineGroup, 'quantity'));
+		$newCartLineGroup = [];
+		$spread = CartPriceUtility::spreadAmountEvenly($newTotal, $quantity);
+		foreach ($spread as $unitPrice => $qty) {
+			$newCartLineGroup[] = [
+				'name' => $cartLineGroup[0]['name'],
+				'quantity' => $qty,
+				'unitPrice' => (float) $unitPrice,
+				'totalAmount' => (float) $unitPrice * $qty,
+				'sku' => isset($cartLineGroup[0]['sku']) ? $cartLineGroup[0]['sku'] : '',
+				'targetVat' => $cartLineGroup[0]['targetVat'],
+				'category' => $cartLineGroup[0]['category'],
+			];
+		}
+
+		return $newCartLineGroup;
+	}
+
+	/**
+	 * @param array $cartItems
+	 * @param $apiRoundingPrecision
+	 * @param $gift_products
+	 * @param array $orderLines
+	 * @param $selectedVoucherCategory
+	 * @param $remaining
+	 *
+	 * @return array
+	 */
+	private function createProductLines(array $cartItems, $apiRoundingPrecision, $gift_products, array $orderLines, $selectedVoucherCategory, $remaining)
+	{
 		foreach ($cartItems as $cartItem) {
 			// Get the rounded total w/ tax
 			$roundedTotalWithTax = round($cartItem['total_wt'], $apiRoundingPrecision);
@@ -105,14 +179,13 @@ class CartLinesService
 			$idCustomization = TextFormatUtility::formatNumber($cartItem['id_customization'], 0);
 
 			$productHash = "{$idProduct}¤{$idProductAttribute}¤{$idCustomization}";
-			$aItems[$productHash] = [];
 
-			foreach ($cartSummary['gift_products'] as $gift_product) {
+			foreach ($gift_products as $gift_product) {
 				if ($gift_product['id_product'] === $cartItem['id_product']) {
 					$quantity = NumberUtility::minus($quantity, $gift_product['cart_quantity']);
 
 					$productHashGift = "{$idProduct}¤{$idProductAttribute}¤{$idCustomization}gift";
-					$aItems[$productHashGift][] = [
+					$orderLines[$productHashGift][] = [
 						'name' => $cartItem['name'],
 						'sku' => $productHashGift,
 						'targetVat' => (float) $cartItem['rate'],
@@ -121,28 +194,42 @@ class CartLinesService
 						'totalAmount' => 0,
 						'category' => '',
 					];
-					break;
+					continue;
 				}
 			}
 
-			// Try to spread this product evenly and account for rounding differences on the order line
-			foreach (CartPriceUtility::spreadAmountEvenly($roundedTotalWithTax, $quantity) as $unitPrice => $qty) {
-				$aItems[$productHash][] = [
-					'name' => $cartItem['name'],
-					'sku' => $productHash,
-					'targetVat' => (float) $cartItem['rate'],
-					'quantity' => $qty,
-					'unitPrice' => $unitPrice,
-					'totalAmount' => (float) NumberUtility::times($unitPrice, $qty),
-					'category' => $this->voucherService->getVoucherCategory($cartItem, $selectedVoucherCategory),
-				];
-				$remaining -= round((float) NumberUtility::times($unitPrice, $qty), $apiRoundingPrecision);
+			if ((int) $quantity <= 0) {
+				continue;
 			}
+
+			// Try to spread this product evenly and account for rounding differences on the order line
+			$orderLines[$productHash][] = [
+				'name' => $cartItem['name'],
+				'sku' => $productHash,
+				'targetVat' => (float) $cartItem['rate'],
+				'quantity' => $quantity,
+				'unitPrice' => round($cartItem['price_wt'], $apiRoundingPrecision),
+				'totalAmount' => (float) $roundedTotalWithTax,
+				'category' => $this->voucherService->getVoucherCategory($cartItem, $selectedVoucherCategory),
+			];
+			$remaining -= $roundedTotalWithTax;
 		}
 
-		// Add discount if applicable
+		return [$orderLines, $remaining];
+	}
+
+	/**
+	 * @param $totalDiscounts
+	 * @param $apiRoundingPrecision
+	 * @param $orderLines
+	 * @param $remaining
+	 *
+	 * @return array
+	 */
+	private function addDiscountsToProductLines($totalDiscounts, $apiRoundingPrecision, $orderLines, $remaining)
+	{
 		if ($totalDiscounts >= 0.01) {
-			$aItems['discount'] = [
+			$orderLines['discount'] = [
 				[
 					'name' => 'Discount',
 					'type' => 'discount',
@@ -156,10 +243,21 @@ class CartLinesService
 			$remaining = NumberUtility::plus($remaining, $totalDiscounts);
 		}
 
-		// Compensate for order total rounding inaccuracies
+		return [$orderLines, $remaining];
+	}
+
+	/**
+	 * @param $remaining
+	 * @param $apiRoundingPrecision
+	 * @param $orderLines
+	 *
+	 * @return array
+	 */
+	private function compositeRoundingInaccuracies($remaining, $apiRoundingPrecision, $orderLines)
+	{
 		$remaining = round($remaining, $apiRoundingPrecision);
 		if ($remaining < 0) {
-			foreach (array_reverse($aItems) as $hash => $items) {
+			foreach (array_reverse($orderLines) as $hash => $items) {
 				// Grab the line group's total amount
 				$totalAmount = array_sum(array_column($items, 'totalAmount'));
 
@@ -173,22 +271,35 @@ class CartLinesService
 
 				// Otherwise spread the cart line again with the updated total
 				//TODO: check why remaining comes -100 when testing and new total becomes different
-				$aItems[$hash] = static::spreadCartLineGroup($items, $totalAmount - $remaining);
+				$orderLines[$hash] = static::spreadCartLineGroup($items, $totalAmount - $remaining);
 				break;
 			}
 		} elseif ($remaining > 0) {
-			foreach (array_reverse($aItems) as $hash => $items) {
+			foreach (array_reverse($orderLines) as $hash => $items) {
 				// Grab the line group's total amount
 				$totalAmount = array_sum(array_column($items, 'totalAmount'));
 				// Otherwise spread the cart line again with the updated total
-				$aItems[$hash] = static::spreadCartLineGroup($items, $totalAmount + $remaining);
+				$orderLines[$hash] = static::spreadCartLineGroup($items, $totalAmount + $remaining);
 				break;
 			}
 		}
 
-		// Fill the order lines with the rest of the data (tax, total amount, etc.)
-		foreach ($aItems as $productHash => $aItem) {
-			$aItems[$productHash] = array_map(function ($line) use ($apiRoundingPrecision, $vatRatePrecision) {
+		return $orderLines;
+	}
+
+	/**
+	 * @param array $orderLines
+	 * @param $apiRoundingPrecision
+	 * @param $vatRatePrecision
+	 *
+	 * @return array
+	 *
+	 * @throws \MolliePrefix\PrestaShop\Decimal\Exception\DivisionByZeroException
+	 */
+	private function fillProductLinesWithRemainingData(array $orderLines, $apiRoundingPrecision, $vatRatePrecision)
+	{
+		foreach ($orderLines as $productHash => $aItem) {
+			$orderLines[$productHash] = array_map(function ($line) use ($apiRoundingPrecision, $vatRatePrecision) {
 				$quantity = (int) $line['quantity'];
 				$targetVat = $line['targetVat'];
 				$unitPrice = $line['unitPrice'];
@@ -200,7 +311,7 @@ class CartLinesService
 				);
 
 				// Calculate VAT
-				$totalAmount = round(NumberUtility::times($unitPrice, $quantity), $apiRoundingPrecision);
+				$totalAmount = $line['totalAmount'];
 				$actualVatRate = 0;
 				if ($unitPriceNoTax > 0) {
 					$actualVatRate = round(
@@ -231,11 +342,23 @@ class CartLinesService
 			}, $aItem);
 		}
 
-		// Add shipping
+		return $orderLines;
+	}
+
+	/**
+	 * @param $shipping
+	 * @param $cartSummary
+	 * @param $apiRoundingPrecision
+	 * @param array $orderLines
+	 *
+	 * @return array
+	 */
+	private function addShippingLine($shipping, $cartSummary, $apiRoundingPrecision, array $orderLines)
+	{
 		if (round($shipping, 2) > 0) {
 			$shippingVatRate = round(($cartSummary['total_shipping'] - $cartSummary['total_shipping_tax_exc']) / $cartSummary['total_shipping_tax_exc'] * 100, $apiRoundingPrecision);
 
-			$aItems['shipping'] = [
+			$orderLines['shipping'] = [
 				[
 					'name' => $this->languageService->lang('Shipping'),
 					'quantity' => 1,
@@ -247,7 +370,20 @@ class CartLinesService
 			];
 		}
 
-		// Add wrapping
+		return $orderLines;
+	}
+
+	/**
+	 * @param $wrapping
+	 * @param array $cartSummary
+	 * @param $vatRatePrecision
+	 * @param $apiRoundingPrecision
+	 * @param array $orderLines
+	 *
+	 * @return array
+	 */
+	private function addWrappingLine($wrapping, array $cartSummary, $vatRatePrecision, $apiRoundingPrecision, array $orderLines)
+	{
 		if (round($wrapping, 2) > 0) {
 			$wrappingVatRate = round(
 				CalculationUtility::getActualVatRate(
@@ -257,7 +393,7 @@ class CartLinesService
 				$vatRatePrecision
 			);
 
-			$aItems['wrapping'] = [
+			$orderLines['wrapping'] = [
 				[
 					'name' => $this->languageService->lang('Gift wrapping'),
 					'quantity' => 1,
@@ -269,9 +405,20 @@ class CartLinesService
 			];
 		}
 
-		// Add fee
+		return $orderLines;
+	}
+
+	/**
+	 * @param $paymentFee
+	 * @param $apiRoundingPrecision
+	 * @param array $orderLines
+	 *
+	 * @return array
+	 */
+	private function addPaymentFeeLine($paymentFee, $apiRoundingPrecision, array $orderLines)
+	{
 		if ($paymentFee) {
-			$aItems['surcharge'] = [
+			$orderLines['surcharge'] = [
 				[
 					'name' => $this->languageService->lang('Payment Fee'),
 					'quantity' => 1,
@@ -283,15 +430,35 @@ class CartLinesService
 			];
 		}
 
-		// Ungroup all the cart lines, just one level
+		return $orderLines;
+	}
+
+	/**
+	 * @param array $orderLines
+	 *
+	 * @return array
+	 */
+	private function ungroupLines(array $orderLines)
+	{
 		$newItems = [];
-		foreach ($aItems as &$items) {
+		foreach ($orderLines as &$items) {
 			foreach ($items as &$item) {
 				$newItems[] = $item;
 			}
 		}
 
-		// Convert floats to strings for the Mollie API and add additional info
+		return $newItems;
+	}
+
+	/**
+	 * @param array $newItems
+	 * @param $currencyIsoCode
+	 * @param $apiRoundingPrecision
+	 *
+	 * @return array
+	 */
+	private function convertToLineArray(array $newItems, $currencyIsoCode, $apiRoundingPrecision)
+	{
 		foreach ($newItems as $index => $item) {
 			$line = new Line();
 			$line->setName($item['name'] ?: $item['sku']);
@@ -333,40 +500,5 @@ class CartLinesService
 		}
 
 		return $newItems;
-	}
-
-	/**
-	 * Spread the cart line amount evenly.
-	 *
-	 * Optionally split into multiple lines in case of rounding inaccuracies
-	 *
-	 * @param array[] $cartLineGroup Cart Line Group WITHOUT VAT details (except target VAT rate)
-	 * @param float $newTotal
-	 *
-	 * @return array[]
-	 *
-	 * @since 3.2.2
-	 * @since 3.3.3 Omits VAT details
-	 */
-	public static function spreadCartLineGroup($cartLineGroup, $newTotal)
-	{
-		$apiRoundingPrecision = Config::API_ROUNDING_PRECISION;
-		$newTotal = round($newTotal, $apiRoundingPrecision);
-		$quantity = array_sum(array_column($cartLineGroup, 'quantity'));
-		$newCartLineGroup = [];
-		$spread = CartPriceUtility::spreadAmountEvenly($newTotal, $quantity);
-		foreach ($spread as $unitPrice => $qty) {
-			$newCartLineGroup[] = [
-				'name' => $cartLineGroup[0]['name'],
-				'quantity' => $qty,
-				'unitPrice' => (float) $unitPrice,
-				'totalAmount' => (float) $unitPrice * $qty,
-				'sku' => isset($cartLineGroup[0]['sku']) ? $cartLineGroup[0]['sku'] : '',
-				'targetVat' => $cartLineGroup[0]['targetVat'],
-				'category' => $cartLineGroup[0]['category'],
-			];
-		}
-
-		return $newCartLineGroup;
 	}
 }
