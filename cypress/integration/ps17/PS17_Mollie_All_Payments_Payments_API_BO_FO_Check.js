de95cfdd10ad2e6e2168a6c83b00591d9eab4c9b
SimonasB88 <simonas.burneikis@invertus.eu>
2021-12-10 19:40:59 +0200

    small test updates

--- cypress/integration/ps17/PS17_Mollie_All_Payments_Payments_API_BO_FO_Check.js
+++ cypress/integration/ps17/PS17_Mollie_All_Payments_Payments_API_BO_FO_Check.js
@@ -170,4 +170,93 @@ it('iDEAL Order BO Refunding [Payments API]', () => {
       cy.get(':nth-child(2) > .swal-button').click()
       cy.get('#mollie_order > :nth-child(1) > .alert').contains('Refund was made successfully!')
 })
+it('Credit card checkout FO [Payments API]', () => {
+      Cypress.on('uncaught:exception', (err, runnable) => {
+      // returning false here prevents Cypress from
+      // failing the test
+      return false
+      })
+      var fasterLoginDE = (LoginFoDE) => {
+      cy.session (LoginFoDE, () => {
+      cy.mollie_1752_test_faster_login_DE_Orders_Api()
+      })
+      }
+      fasterLoginDE('LoginFoDE')
+      cy.visit('https://demo.invertus.eu/clients/mollie17-test/en/home/21-testproduct1.html')
+      cy.get('.add > .btn').click()
+      cy.get('.cart-content-btn > .btn-primary').click()
+      cy.get('.text-sm-center > .btn').click()
+      cy.contains('Germany').click()
+      cy.get('.clearfix > .btn').click()
+      cy.get('#js-delivery > .continue').click()
+      cy.contains('Credit card').click({force:true})
+      //Credit card inputing
+      cy.frameLoaded('[data-testid=mollie-container--cardHolder] > iframe')
+      cy.enter('[data-testid=mollie-container--cardHolder] > iframe').then(getBody => {
+      getBody().find('#cardHolder').type('TEST TEEESSSTT')
+      })
+      cy.enter('[data-testid=mollie-container--cardNumber] > iframe').then(getBody => {
+      getBody().find('#cardNumber').type('5555555555554444')
+      })
+      cy.enter('[data-testid=mollie-container--expiryDate] > iframe').then(getBody => {
+      getBody().find('#expiryDate').type('1222')
+      })
+      cy.enter('[data-testid=mollie-container--verificationCode] > iframe').then(getBody => {
+      getBody().find('#verificationCode').type('222')
+      })
+      cy.get('.ps-shown-by-js > .btn').click()
+      cy.setCookie(
+        'SESSIONID',
+        "cypress-dummy-value",
+        {
+            domain: '.www.mollie.com',
+            sameSite: 'None',
+            secure: true,
+            httpOnly: true
+        }
+      );    // reload current page to activate cookie
+      cy.reload();
+      cy.get(':nth-child(2) > .checkbox > .checkbox__label').click()
+      cy.get('.button').click()
+      //Success page UI verification
+      cy.get('.h1').should('include.text','Your order is confirmed')
+      cy.get('#order-details > ul > :nth-child(2)').should('include.text','Credit Card', {matchCase: false})
+})
+it('Credit card Order BO Shiping, Refunding [Payments API]', () => {
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+  })
+  var login = (MollieBOLoggingIn) => {
+    cy.session(MollieBOLoggingIn,() => {
+    cy.mollie_1752_test_demo_module_dashboard()
+    cy.mollie_1752_test_login()
+    })
+    }
+  login('MollieBOLoggingIn')
+      cy.visit('https://demo.invertus.eu/clients/mollie17-test/admin1/index.php?controller=AdminOrders')
+      cy.get('tbody > :nth-child(1) > :nth-child(9)').should('include.text','Payment accepted')
+      cy.get(':nth-child(1) > :nth-child(15) > .btn-group > .btn').click()
+      cy.get('#mollie_order > :nth-child(1)').should('exist')
+      cy.get('.form-inline > :nth-child(1) > .btn').should('exist')
+      cy.get('.input-group-btn > .btn').should('exist')
+      cy.get('.sc-htpNat > .panel > .card-body > :nth-child(3)').should('exist')
+      cy.get('.card-body > :nth-child(6)').should('exist')
+      cy.get('.card-body > :nth-child(9)').should('exist')
+      cy.get('#mollie_order > :nth-child(1) > :nth-child(1)').should('exist')
+      cy.get('.sc-htpNat > .panel > .card-body').should('exist')
+      cy.get('.sc-bxivhb > .panel > .panel-heading').should('exist')
+      cy.get('.sc-bxivhb > .panel > .card-body').should('exist')
+      //check partial refunding on Payments API
+      cy.get('.form-inline > :nth-child(2) > .input-group > .form-control').type('1.51',{delay:0})
+      cy.get(':nth-child(2) > .input-group > .input-group-btn > .btn').click()
+      cy.get('.swal-modal').should('exist')
+      cy.get(':nth-child(2) > .swal-button').click()
+      cy.get('#mollie_order > :nth-child(1) > .alert').contains('Refund was made successfully!')
+      cy.get('.form-inline > :nth-child(1) > .btn').click()
+      cy.get('.swal-modal').should('exist')
+      cy.get(':nth-child(2) > .swal-button').click()
+      cy.get('#mollie_order > :nth-child(1) > .alert').contains('Refund was made successfully!')
+})
 })
