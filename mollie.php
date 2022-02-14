3ae3fca25e34064b1a49460d3692a4874d726e43
margud <marius.gudauskis@invertus.eu>
2022-02-14 17:11:30 +0200

    changed version

--- mollie.php
+++ mollie.php
@@ -44,7 +44,7 @@ class Mollie extends PaymentModule
     {
         $this->name = 'mollie';
         $this->tab = 'payments_gateways';
-        $this->version = '5.0.0';
+        $this->version = '5.0.1';
         $this->author = 'Mollie B.V.';
         $this->need_instance = 1;
         $this->bootstrap = true;
