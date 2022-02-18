e1d3364cc2b7bf9d5f42816a91573a34c4d8c2b3
SimonasB88 <simonas.burneikis@invertus.eu>
2022-02-18 18:34:05 +0200

    test update

--- cypress/integration/local/index.js
+++ cypress/integration/local/index.js
@@ -8,6 +8,6 @@ it('Access BO, check Mollie shortcut', () => {
     cy.get('#email').type('demo@demo.com',{delay: 0, log: false})
     cy.get('#passwd').type('demodemo',{delay: 0, log: false})
     cy.get('#submit_login').click()
-    cy.url().should('contain','https').screenshot()
+    cy.url().should('contain','https')
 })
 })
