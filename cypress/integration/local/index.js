aecf2e90b3c46ce5985eb7a3ac1dd1e48a3fb7a6
SimonasB88 <simonas.burneikis@invertus.eu>
2022-02-18 17:12:20 +0200

    test update

--- cypress/integration/local/index.js
+++ cypress/integration/local/index.js
@@ -1,10 +1,7 @@
-it('Should check if home page is loaded', () => {
-    cy.request({
-        url: 'https://demoshop.ngrok.io/',
-    }).then((resp) => {
-        expect(resp.status).to.eq(200)
-    })
-})
+/// <reference types="Cypress" />
+context('Cypress test', () => {
+  beforeEach(() => {   
+  })
 it('Access BO, check Mollie shortcut', () => {
     cy.visit('https://demoshop.ngrok.io/admin1/')
     cy.get('#email').type('demo@demo.com',{delay: 0, log: false})
