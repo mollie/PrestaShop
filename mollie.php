f0aacc1ddae87d77dc7d17d41a1ed209b709aaab
margud <marius.gudauskis@invertus.eu>
2022-10-18 13:55:01 +0300

    MOLIM-396: fixed components tab selection when card is saved

--- mollie.php
+++ mollie.php
@@ -50,7 +50,7 @@ class Mollie extends PaymentModule
     {
         $this->name = 'mollie';
         $this->tab = 'payments_gateways';
-        $this->version = '5.3.1';
+        $this->version = '5.4.0';
         $this->author = 'Mollie B.V.';
         $this->need_instance = 1;
         $this->bootstrap = true;
