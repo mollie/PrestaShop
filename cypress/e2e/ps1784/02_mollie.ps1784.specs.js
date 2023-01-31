bec9f58b4fde5e199d0418589c1b2fca05467268
SimonasB88 <simonas.burneikis@invertus.eu>
2023-01-31 14:26:58 +0200

    Update 02_mollie.ps1784.specs.js

--- cypress/e2e/ps1784/02_mollie.ps1784.specs.js
+++ cypress/e2e/ps1784/02_mollie.ps1784.specs.js
@@ -55,14 +55,9 @@ let windowConsoleError;
 Cypress.on('window:before:load', (win) => {
   windowConsoleError = cy.spy(win.console, 'error');
 })
-let failEarly = false;
 afterEach(() => {
   expect(windowConsoleError).to.not.be.called;
-  if (failEarly) throw new Error("Failing Early due to a fail somewhere in the workflow. If running on CI, please check Cypress VIDEOS/SCREENSHOTS in the Artifacts for more details.")
 })
-afterEach(function() {
-  if (this.currentTest.state === "failed") failEarly = true
-});
 describe('PS1784 Tests Suite', () => {
   beforeEach(() => {
       cy.viewport(1920,1080)
