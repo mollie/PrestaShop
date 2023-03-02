45bf039ee1af88f80fb761957c737536f4e8d831
SimonasB88 <simonas.burneikis@invertus.eu>
2023-03-02 12:50:40 +0200

    Create 03_mollie.ps1784.Subscriptions.js

---
+++ cypress/e2e/ps1784/03_mollie.ps1784.Subscriptions.js
@@ -0,0 +1,47 @@
+//Caching the BO and FO session
+const login = (MollieBOFOLoggingIn) => {
+  cy.session(MollieBOFOLoggingIn,() => {
+  cy.visit('/admin1/')
+  cy.url().should('contain', 'https').as('Check if HTTPS exists')
+  cy.get('#email').type('demo@demo.com',{delay: 0, log: false})
+  cy.get('#passwd').type('demodemo',{delay: 0, log: false})
+  cy.get('#submit_login').click().wait(1000).as('Connection successsful')
+  //switching the multistore PS1784
+  cy.get('#header_shop > .dropdown').click()
+  cy.get('.open > .dropdown-menu').find('[class="shop"]').eq(1).find('[href]').eq(0).click()
+  cy.visit('/SHOP2/index.php?controller=my-account')
+  cy.get('#login-form [name="email"]').eq(0).type('demo@demo.com')
+  cy.get('#login-form [name="password"]').eq(0).type('demodemo')
+  cy.get('#login-form [type="submit"]').eq(0).click({force:true})
+  cy.get('#history-link > .link-item').click()
+  })
+  }
+
+//Checing the console for errors
+let windowConsoleError;
+Cypress.on('window:before:load', (win) => {
+windowConsoleError = cy.spy(win.console, 'error');
+})
+afterEach(() => {
+expect(windowConsoleError).to.not.be.called;
+})
+describe('PS1784 Tests Suite', () => {
+  beforeEach(() => {
+      cy.viewport(1920,1080)
+      login('MollieBOFOLoggingIn')
+  })
+it.only('Check if Subscription options are in Product Page', () => {
+  cy.visit('/admin1/')
+  cy.get('#subtab-AdminCatalog > :nth-child(1)').click()
+  cy.get('#subtab-AdminProducts > .link').click()
+  cy.contains('Hummingbird printed t-shirt').click()
+  cy.get('[id="tab_step3"]').click()
+  cy.contains('Daily').click({force:true})
+  cy.wait(10000)
+  cy.get('[class="token"]').should('be.visible')
+  cy.get('#create-combinations').click()
+  cy.wait(5000)
+  cy.contains('Mollie subscription - Daily').should('be.visible')
+  cy.get('#attribute_42 > .attribute-quantity > div > .form-control').clear().type('999')
+});
+})
