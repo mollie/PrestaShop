105fdf36108d7849314890f75b02e74cf72c1808
mandan2 <mantasda2@gmail.com>
2023-03-06 17:06:14 +0200

    PIPRES-94: Configuration settings based on environment

--- src/Adapter/ConfigurationAdapter.php
+++ src/Adapter/ConfigurationAdapter.php
@@ -12,35 +12,105 @@
 
 namespace Mollie\Adapter;
 
+use Configuration;
 use Context;
+use Mollie\Config\Config;
 use Shop;
 
 class ConfigurationAdapter
 {
-    public function get($key, $idShop = null, $idLang = null, $idShopGroup = null)
+    /** @var GlobalShopContextInterface */
+    private $globalShopContext;
+
+    public function __construct(GlobalShopContextInterface $globalShopContext)
+    {
+        $this->globalShopContext = $globalShopContext;
+    }
+
+    public function get(string $key, int $idShop = null, int $idLang = null, int $idShopGroup = null): ?string
     {
         if (!$idShop) {
             $idShop = Context::getContext()->shop->id;
+        }
+
+        if (!$idLang) {
+            $idLang = Context::getContext()->shop->id_shop_group;
         }
 
         if (!$idShopGroup) {
             $idShopGroup = Context::getContext()->shop->id_shop_group;
         }
 
-        return \Configuration::get($key, $idLang, $idShopGroup, $idShop);
+        return Configuration::get($key, $idLang, $idShopGroup, $idShop) ?: null;
     }
 
-    public function updateValue($key, $values, $idShop = null, $html = false, $idShopGroup = null)
+    public function updateValue(string $key, string $values, bool $html = false, int $idShop = null, int $idShopGroup = null): void
     {
         if ($idShop === null) {
             $shops = Shop::getShops(true);
+
             foreach ($shops as $shop) {
-                \Configuration::updateValue($key, $values, $html, $shop['id_shop_group'], $shop['id_shop']);
+                Configuration::updateValue($key, $values, $html, $shop['id_shop_group'], $shop['id_shop']);
             }
 
             return;
         }
 
-        \Configuration::updateValue($key, $values, $html, $idShopGroup, $idShop);
+        Configuration::updateValue($key, $values, $html, $idShopGroup, $idShop);
+    }
+
+    /**
+     * @param array{sandbox: string, production: string} $idByEnvironment
+     *
+     * @param mixed $value
+     * @param bool $html
+     * @param int|null $idShop
+     * @param int|null $idShopGroup
+     */
+    public function setByEnvironment(array $idByEnvironment, string $value, bool $html = false, int $idShop = null, int $idShopGroup = null): void
+    {
+        if (!$idShop) {
+            $idShop = $this->globalShopContext->getShopId();
+        }
+
+        if ((int) $this->get(Config::MOLLIE_ENVIRONMENT)) {
+            $id = $idByEnvironment['production'];
+        } else {
+            $id = $idByEnvironment['sandbox'];
+        }
+
+        Configuration::updateValue($id, $value, $html, $idShopGroup, $idShop);
+    }
+
+    /**
+     * @param array{sandbox: string, production: string} $idByEnvironment
+     *
+     * @param int|null $idShop
+     * @param int|null $idLang
+     * @param int|null $idShopGroup
+     *
+     * @return string|null
+     */
+    public function getByEnvironment(array $idByEnvironment, int $idShop = null, int $idLang = null, int $idShopGroup = null): ?string
+    {
+        if (!$idShop) {
+            $idShop = Context::getContext()->shop->id;
+        }
+
+        if (!$idLang) {
+            $idLang = Context::getContext()->shop->id_shop_group;
+        }
+
+        if (!$idShopGroup) {
+            $idShopGroup = Context::getContext()->shop->id_shop_group;
+        }
+
+        if ((int) $this->get(Config::MOLLIE_ENVIRONMENT)) {
+            $id = $idByEnvironment['production'];
+        } else {
+            $id = $idByEnvironment['sandbox'];
+        }
+
+        return Configuration::get($id, $idLang, $idShopGroup, $idShop) ?: null;
     }
 }
