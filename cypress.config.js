267c02ed0796b0bd242f123bdd98e3cdcb601974
SimonasB88 <simonas.burneikis@invertus.eu>
2023-03-07 13:05:36 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  cacheAcrossSpecs: true,
   chromeWebSecurity: false,
   experimentalSourceRewriting: true,
   numTestsKeptInMemory: 5,
