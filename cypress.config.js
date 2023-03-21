5f420d377056b2734bb87fe968fd737599a15212
SimonasB88 <simonas.burneikis@invertus.eu>
2023-03-21 17:51:01 +0200

    small cypress.config update

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,9 @@ module.exports = defineConfig({
   chromeWebSecurity: false,
   experimentalSourceRewriting: true,
   numTestsKeptInMemory: 5,
-  defaultCommandTimeout: 8000,
+  defaultCommandTimeout: 7000,
   projectId: 'xb89dr',
-  retries: 2,
+  retries: 1,
   videoUploadOnPasses: false,
   videoCompression: 15,
   e2e: {
