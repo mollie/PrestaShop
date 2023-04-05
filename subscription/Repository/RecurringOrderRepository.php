3479c2837713ed6fb7d7a9f444c7de501a050322
mandan2 <mantasda2@gmail.com>
2023-04-05 10:17:51 +0300

    csfixer fixes

--- subscription/Repository/RecurringOrderRepository.php
+++ subscription/Repository/RecurringOrderRepository.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Mollie\Subscription\Repository;
 
 use MolRecurringOrder;
-use PrestaShop\PrestaShop\Adapter\Entity\Db;
-use PrestaShop\PrestaShop\Adapter\Entity\DbQuery;
 use PrestaShopCollection;
 
 class RecurringOrderRepository extends AbstractRepository implements RecurringOrderRepositoryInterface
