2332bf75b57fc32c912d297098c57857503ba225
mandan2 <mantasda2@gmail.com>
2023-04-05 10:13:41 +0300

    remove not used repository method

--- subscription/Repository/RecurringOrderRepository.php
+++ subscription/Repository/RecurringOrderRepository.php
@@ -26,23 +26,4 @@ class RecurringOrderRepository extends AbstractRepository implements RecurringOr
 
         return $result;
     }
-
-    public function getCustomerOrdersByOldAddress(int $customerId, int $oldAddressId): ?array
-    {
-        $sql = new DbQuery();
-
-        $sql->select('o.id_order, o.id_address_delivery, o.id_address_invoice');
-        $sql->from('mol_recurring_order', 'mro');
-        $sql->leftJoin('orders', 'o', 'mro.id_order = o.id_order');
-        $sql->where('mro.id_customer = ' . $customerId);
-        $sql->where('o.id_address_delivery = ' . $oldAddressId . ' OR o.id_address_invoice = ' . $oldAddressId);
-
-        $result = Db::getInstance()->executeS($sql);
-
-        if (is_array($result) && !empty($result)) {
-            return $result;
-        }
-
-        return null;
-    }
 }
