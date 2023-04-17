397a183d235a1304cdad20feeeb9890913d1aacc
SimonasB88 <simonas.burneikis@invertus.eu>
2023-04-17 16:06:20 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -11,6 +11,7 @@ module.exports = defineConfig({
   videoCompression: 8,
   viewportHeight: 1080,
   viewportWidth: 1920,
+  experimentalMemoryManagement: true,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
