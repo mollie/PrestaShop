3521e75e028415b02e45569b23dd1f3a8d4ed398
SimonasB88 <simonas.burneikis@invertus.eu>
2023-04-24 10:39:17 +0300

    Update parameters.php

--- tests/seed/settings8/parameters.php
+++ tests/seed/settings8/parameters.php
@@ -1,6 +1,7 @@
-<?php return array (
-  'parameters' =>
-  array (
+<?php
+
+return [
+  'parameters' => [
     'database_host' => 'mysql',
     'database_port' => '3306',
     'database_name' => 'prestashop',
@@ -10,8 +11,8 @@
     'database_engine' => 'InnoDB',
     'mailer_transport' => 'smtp',
     'mailer_host' => '127.0.0.1',
-    'mailer_user' => NULL,
-    'mailer_password' => NULL,
+    'mailer_user' => null,
+    'mailer_password' => null,
     'secret' => '${PS_SECRET}',
     'ps_caching' => 'CacheMemcache',
     'ps_cache_enable' => false,
@@ -21,5 +22,5 @@
     'cookie_key' => '6NiLzzVp8Z9LiOtHgBNScv929I2K8a2A8jNppJVz1t5WjnnRGPbXVXKS',
     'cookie_iv' => 'TtPM0O7S',
     'new_cookie_key' => 'def00000332ef14cde8a65804d08f17a87b5d415f87c37c3c706c29a765858e114ca8b80d96360f04dda31690be7df716bc41294218377ee51e61ac751a160aece8e6725',
-  ),
-);
+  ],
+];
