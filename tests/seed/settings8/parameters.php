98300ec17a8fc20d8c7d61f8384b62fb2fd04ecf
SimonasB88 <simonas.burneikis@invertus.eu>
2023-05-09 10:26:46 +0300

    adding ps8 CI

--- tests/seed/settings8/parameters.php
+++ tests/seed/settings8/parameters.php
@@ -3,7 +3,7 @@
 return [
   'parameters' => [
     'database_host' => 'mysql',
-    'database_port' => '',
+    'database_port' => '3306',
     'database_name' => 'prestashop',
     'database_user' => 'root',
     'database_password' => 'prestashop',
@@ -16,11 +16,11 @@ return [
     'secret' => '$${PS_SECRET}',
     'ps_caching' => 'CacheMemcache',
     'ps_cache_enable' => false,
-    'ps_creation_date' => '2020-12-23',
+    'ps_creation_date' => '2023-02-21',
     'locale' => 'en-US',
     'use_debug_toolbar' => true,
     'cookie_key' => '6NiLzzVp8Z9LiOtHgBNScv929I2K8a2A8jNppJVz1t5WjnnRGPbXVXKS',
     'cookie_iv' => 'TtPM0O7S',
-    'new_cookie_key' => 'def000001b90465d16f2aa760abe76e1a88fb80071075978db96754d16fd83cef2c9d8d1218a6dba2f28cee48e527700e85ddf87731652776449d60f9f4e1cceb90198b2',
+    'new_cookie_key' => 'def00000332ef14cde8a65804d08f17a87b5d415f87c37c3c706c29a765858e114ca8b80d96360f04dda31690be7df716bc41294218377ee51e61ac751a160aece8e6725',
   ],
 ];
