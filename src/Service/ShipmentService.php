e98ec89fbc572951252f68341098458e9e020555
mandan2 <mantasda2@gmail.com>
2023-06-06 11:04:09 +0300

    minor improvements

--- src/Service/ShipmentService.php
+++ src/Service/ShipmentService.php
@@ -64,15 +64,21 @@ class ShipmentService implements ShipmentServiceInterface
     public function getShipmentInformation($orderReference)
     {
         $orders = Order::getByReference($orderReference);
+
         /** @var Order $order */
         $order = $orders->getFirst();
+
         if (!Validate::isLoadedObject($order)) {
             return null;
         }
+
         $invoiceAddress = new Address($order->id_address_invoice);
         $deliveryAddress = new Address($order->id_address_delivery);
+
         $carrierInformationId = $this->informationRepository->getMollieCarrierInformationIdByCarrierId($order->id_carrier);
+
         $carrierInformation = new MolCarrierInformation($carrierInformationId);
+
         if (!Validate::isLoadedObject($invoiceAddress)
             || !Validate::isLoadedObject($deliveryAddress)
             || !Validate::isLoadedObject($carrierInformation)
@@ -85,17 +91,21 @@ class ShipmentService implements ShipmentServiceInterface
         }
 
         $langId = Context::getContext()->language->id;
+
         if (Config::MOLLIE_CARRIER_MODULE === $carrierInformation->url_source) {
             $carrier = new Carrier($order->id_carrier);
+
             if (in_array($carrier->external_module_name, ['postnl', 'myparcel'])) {
                 $table = 'postnl' === $carrier->external_module_name ? 'postnlmod_order' : 'myparcel_order';
 
                 try {
                     $info = $this->orderShipmentRepository->getShipmentInformation($table, $order->id);
+
                     if ($info['tracktrace'] && $info['postcode']) {
                         $postcode = Tools::strtoupper(str_replace(' ', '', $info['postcode']));
                         $langIso = Tools::strtoupper(Language::getIsoById($langId));
                         $countryIso = Tools::strtoupper(Country::getIsoById($deliveryAddress->id_country));
+
                         $tracktrace = $info['tracktrace'];
 
                         return [
@@ -121,17 +131,15 @@ class ShipmentService implements ShipmentServiceInterface
             $carrier = new Carrier($order->id_carrier);
             $orderCarrier = new OrderCarrier($order->getIdOrderCarrier());
 
-            $shippingNumber = $orderCarrier->tracking_number;
-
-            if (!$shippingNumber || !$carrier->name) {
+            if (!$carrier->id || !$orderCarrier->id || !$carrier->name || !$orderCarrier->tracking_number) {
                 return [];
             }
 
             return [
                 'tracking' => [
                     'carrier' => $carrier->name,
-                    'code' => $shippingNumber,
-                    'url' => str_replace('@', $shippingNumber, $carrier->url),
+                    'code' => $orderCarrier->tracking_number,
+                    'url' => str_replace('@', $orderCarrier->tracking_number, $carrier->url),
                 ],
             ];
         }
@@ -140,7 +148,9 @@ class ShipmentService implements ShipmentServiceInterface
             $carrier = new Carrier($order->id_carrier);
             $orderCarrier = new OrderCarrier($order->getIdOrderCarrier());
 
-            $shippingNumber = $orderCarrier->tracking_number;
+            if (!$carrier->id || !$orderCarrier->id || !$carrier->name || !$orderCarrier->tracking_number) {
+                return [];
+            }
 
             $invoicePostcode = Tools::strtoupper(str_replace(' ', '', $invoiceAddress->postcode));
             $invoiceCountryIso = Tools::strtoupper(Country::getIsoById($invoiceAddress->id_country));
@@ -149,13 +159,9 @@ class ShipmentService implements ShipmentServiceInterface
 
             $langIso = Tools::strtoupper(Language::getIsoById($langId));
 
-            if (!$carrier->name || !$shippingNumber) {
-                return [];
-            }
-
             $info = [
-                '@' => $shippingNumber,
-                '%%shipping_number%%' => $shippingNumber,
+                '@' => $orderCarrier->tracking_number,
+                '%%shipping_number%%' => $orderCarrier->tracking_number,
                 '%%invoice.country_iso%%' => $invoiceCountryIso,
                 '%%invoice.postcode%%' => $invoicePostcode,
                 '%%delivery.country_iso%%' => $deliveryCountryIso,
@@ -166,7 +172,7 @@ class ShipmentService implements ShipmentServiceInterface
             return [
                 'tracking' => [
                     'carrier' => $carrier->name,
-                    'code' => $shippingNumber,
+                    'code' => $orderCarrier->tracking_number,
                     'url' => str_ireplace(
                         array_keys($info),
                         array_values($info),
