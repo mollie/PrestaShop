4e87dd8f3edaaaab54771c3be282b08567556577
mandan2 <mantasda2@gmail.com>
2023-07-25 18:03:35 +0300

    final mollie exception constructor

--- src/Exception/MollieException.php
+++ src/Exception/MollieException.php
@@ -14,12 +14,18 @@ namespace Mollie\Exception;
 
 use Exception;
 use Mollie\Exception\Code\ExceptionCode;
+use Throwable;
 
 class MollieException extends \Exception
 {
     const CUSTOMER_EXCEPTION = 1;
 
     const API_CONNECTION_EXCEPTION = 2;
+
+    final public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 
     public static function unknownError(Exception $exception): self
     {
