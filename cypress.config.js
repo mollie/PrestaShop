0e6f208009598653cdbab36e402060ce16761228
SimonasB88 <simonas.burneikis@invertus.eu>
2023-07-31 14:53:53 +0300

    useful E2E test spec updates

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   chromeWebSecurity: false,
   experimentalMemoryManagement: true,
   experimentalSourceRewriting: true,
-  numTestsKeptInMemory: 3,
+  numTestsKeptInMemory: 0,
   defaultCommandTimeout: 15000,
   projectId: 'xb89dr',
   retries: 3,
