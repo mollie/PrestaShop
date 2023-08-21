2eea085f809238b8bfbe8e44f7eb66790cfdecc3
SimonasB88 <simonas.burneikis@invertus.eu>
2023-08-21 12:27:32 +0300

    ps8 test spec updates

--- cypress/e2e/ps8/02_mollie.ps8.specs.js
+++ cypress/e2e/ps8/02_mollie.ps8.specs.js
@@ -31,28 +31,11 @@ function prepareCookie()
 
         });
       }
-      //Catching the BO and FO session
-      const login = (MollieBOFOLoggingIn) => {
-      cy.session(MollieBOFOLoggingIn,() => {
-      cy.visit('/admin1/')
-      cy.url().should('contain', 'https').as('Check if HTTPS exists')
-      cy.get('#email').type('demo@prestashop.com',{delay: 0, log: false})
-      cy.get('#passwd').type('prestashop_demo',{delay: 0, log: false})
-      cy.get('#submit_login').click().wait(1000).as('Connection successsful')
-      //switching the multistore PS8
-      cy.get('#header_shop > .dropdown').click()
-      cy.get('.open > .dropdown-menu').find('[class="shop"]').eq(1).find('[href]').eq(0).click()
-      cy.visit('/SHOP2/index.php?controller=my-account')
-      cy.get('#login-form [name="email"]').eq(0).type('demo@demo.com')
-      cy.get('#login-form [name="password"]').eq(0).type('prestashop_demo')
-      cy.get('#login-form [type="submit"]').eq(0).click({force:true})
-      cy.get('#history-link > .link-item').click()
-      })
-      }
+
 describe('PS8 Tests Suite', () => {
   beforeEach(() => {
       cy.viewport(1920,1080)
-      login('MollieBOFOLoggingIn')
+      cy.CachingBOFOPS8()
   })
 it('C339341: 04 Enabling All payments in Module BO [Orders API]', () => {
       cy.visit('/admin1/')
@@ -64,7 +47,7 @@ it('C339341: 04 Enabling All payments in Module BO [Orders API]', () => {
 })
 // Somehow the Payment is not appearing in the ecommerce frontend...needs to be checked...TODO
 it.skip('C339342: 05 Vouchers Checkouting [Orders API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       cy.contains('Reorder').click()
       cy.contains('DE').click()
       //Billing country LT, DE etc.
@@ -99,7 +82,7 @@ it.skip('C339343: 06 Vouchers Order BO Refunding, Shipping (Paid part only) [Ord
       cy.get('[class="card-body"]').find('[class="alert alert-warning"]').should('exist') //additional checking if the warning alert for vouchers exist
 })
 it('C339344: 07 Bancontact Checkouting [Orders API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       cy.contains('Reorder').click()
       cy.contains('DE').click()
       //Billing country LT, DE etc.
@@ -129,7 +112,7 @@ it('C339345: 08 Bancontact Order BO Shipping, Refunding [Orders API]', () => {
       cy.OrderRefundingShippingOrdersAPI()
 })
 it('C339346: 09 iDEAL Checkouting [Orders API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       cy.contains('Reorder').click()
       //Billing country LT, DE etc.
       cy.get('.clearfix > .btn').click()
@@ -159,7 +142,7 @@ it('C339347: 10 iDEAL Order BO Shipping, Refunding [Orders API]', () => {
       cy.OrderRefundingShippingOrdersAPI()
 })
 it('C339348: 11 Klarna Slice It Checkouting [Orders API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       cy.contains('Reorder').click()
       //Billing country LT, DE etc.
       cy.contains('DE').click()
@@ -189,7 +172,7 @@ it('C339349: 12 Klarna Slice It Order BO Shipping, Refunding [Orders API]', () =
       cy.OrderShippingRefundingOrdersAPI()
 })
 it('C339350: 13 Klarna Pay Later Checkouting [Orders API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       //
       cy.contains('Reorder').click()
       //Billing country LT, DE etc.
@@ -220,7 +203,7 @@ it('C339351: 14 Klarna Pay Later Order BO Shipping, Refunding [Orders API]', () 
       cy.OrderShippingRefundingOrdersAPI()
 })
 it('C339352: 15 Klarna Pay Now Checkouting [Orders API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       //
       cy.contains('Reorder').click()
       //Billing country LT, DE etc.
@@ -258,7 +241,7 @@ it('C339354: 17 Credit Card Checkouting [Orders API]', () => {
       cy.get('#MOLLIE_SANDBOX_SINGLE_CLICK_PAYMENT_on').click({force:true})
       cy.get('[type="submit"]').first().click({force:true})
       cy.get('[class="alert alert-success"]').should('be.visible')
-      cy.visit('/SHOP2/en/index.php?controller=history')
+      cy.visit('/en/index.php?controller=history')
       cy.contains('Reorder').click()
       //Billing country LT, DE etc.
       cy.get('.clearfix > .btn').click()
@@ -287,7 +270,7 @@ it('C339354: 17 Credit Card Checkouting [Orders API]', () => {
       cy.get('#content-hook_order_confirmation > .card-block').should('be.visible')
 })
 it('C339355: 18 Check if customerId is passed during the 2nd payment using Single Click Payment [Orders API]', () => {
-      cy.visit('/SHOP2/en/index.php?controller=history')
+      cy.visit('/en/index.php?controller=history')
       cy.contains('Reorder').click()
       //Billing country LT, DE etc.
       cy.get('.clearfix > .btn').click()
@@ -320,7 +303,7 @@ it('C339356: 19 Credit Card Order BO Shipping, Refunding [Orders API]', () => {
       cy.OrderRefundingShippingOrdersAPI()
 })
 it('C339357: 20 IN3 Checkouting [Orders API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       cy.contains('Reorder').click()
       cy.contains('NL').click()
       //Billing country LT, DE etc.
@@ -351,7 +334,7 @@ it('C339358: 21 IN3 Order BO Shipping, Refunding [Orders API]', () => {
       cy.OrderRefundingShippingOrdersAPI()
 })
 it('C339359: 22 IN3 should not be shown under 5000 EUR [Orders API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       cy.contains('Reorder').click()
       cy.get('.logo').click()
       cy.get('.blockcart').click()
@@ -376,7 +359,7 @@ it('C339360: 23 IN3 Checking that IN3 logo exists OK [Orders API]', () => {
       cy.get('[name="MOLLIE_IMAGES"]').select('big',{force:true})
       cy.get('[type="submit"]').first().click({force:true})
       cy.get('[class="alert alert-success"]').should('be.visible')
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       cy.contains('Reorder').click()
       cy.contains('NL').click()
       //Billing country LT, DE etc.
@@ -394,7 +377,7 @@ it('C339360: 23 IN3 Checking that IN3 logo exists OK [Orders API]', () => {
       cy.get('[class="alert alert-success"]').should('be.visible')
 })
 it('C339361: 24 Paypal Checkouting [Orders API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       cy.contains('Reorder').click()
       cy.contains('NL').click()
       //Billing country LT, DE etc.
@@ -424,7 +407,7 @@ it('C339362: 25 Paypal Order Shipping, Refunding [Orders API]', () => {
       cy.OrderRefundingShippingOrdersAPI()
 })
 it('C339363: 26 SOFORT Checkouting [Orders API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       cy.contains('Reorder').click()
       cy.contains('NL').click()
       //Billing country LT, DE etc.
@@ -466,7 +449,7 @@ it('C339364: 27 SOFORT Order Shipping, Refunding [Orders API]', () => {
           //Refunding not possible because "We haven't received the payment on our bank accounts yet" message from Mollie Dashboard
 })
 it('C339365: 28 Przelewy24 Checkouting [Orders API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       cy.contains('Reorder').click()
       cy.contains('NL').click()
       //Billing country LT, DE etc.
@@ -496,7 +479,7 @@ it('C339366: 29 Przelewy24 Order Shipping, Refunding [Orders API]', () => {
       cy.OrderRefundingShippingOrdersAPI()
 })
 it('C339367: 30 Giropay Checkouting [Orders API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       cy.contains('Reorder').click()
       cy.contains('NL').click()
       //Billing country LT, DE etc.
@@ -526,7 +509,7 @@ it('C339368: 31 Giropay Order Shipping, Refunding [Orders API]', () => {
   cy.OrderRefundingShippingOrdersAPI()
 })
 it('C339369: 32 EPS Checkouting [Orders API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       cy.contains('Reorder').click()
       cy.contains('NL').click()
       //Billing country LT, DE etc.
@@ -556,7 +539,7 @@ it('C339370: 33 EPS Order Shipping, Refunding [Orders API]', () => {
       cy.OrderRefundingShippingOrdersAPI()
 })
 it('C339371: 34 KBC/CBC Checkouting [Orders API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       cy.contains('Reorder').click()
       cy.contains('NL').click()
       //Billing country LT, DE etc.
@@ -587,7 +570,7 @@ it('C339372: 35 KBC/CBC Order Shipping, Refunding [Orders API]', () => {
       cy.OrderRefundingShippingOrdersAPI()
 })
 it('C339373: 36 Belfius Checkouting [Orders API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       cy.contains('Reorder').click()
       cy.contains('NL').click()
       //Billing country LT, DE etc.
@@ -617,7 +600,7 @@ it('C339374: 37 Belfius Order Shipping, Refunding [Orders API]', () => {
       cy.OrderRefundingShippingOrdersAPI()
 })
 it('C339375: 38 Bank Transfer Checkouting [Orders API]', () => {
-      cy.visit('/SHOP2/en/index.php?controller=history')
+      cy.visit('/en/index.php?controller=history')
       cy.contains('Reorder').click()
       cy.contains('NL').click()
       //Billing country LT, DE etc.
@@ -649,7 +632,7 @@ it('C339376: 39 Bank Transfer Order Shipping, Refunding [Orders API]', () => {
 })
 // Temporary disabled, Payment Method disables automatically in My Mollie Dashboard, because of the fake testing account...
 it.skip('40 Gift Card Checkouting [Orders API]', () => {
-      cy.visit('/SHOP2/en/index.php?controller=history')
+      cy.visit('/en/index.php?controller=history')
       cy.contains('Reorder').click()
       cy.contains('NL').click()
       //Billing country LT, DE etc.
@@ -697,7 +680,7 @@ it('C339378: 43 Check if Bancontact QR payment dropdown exists [Payments API]', 
       cy.get('[name="MOLLIE_BANCONTACT_QR_CODE_ENABLED"]').should('exist')
 })
 it('C339379: 44 Bancontact Checkouting [Payments API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       //
       cy.contains('Reorder').click()
       cy.contains('DE').click()
@@ -728,7 +711,7 @@ it('C339380: 45 Bancontact Order BO Refunding, Partial Refunding [Payments API]'
       cy.OrderRefundingPartialPaymentsAPI()
 })
 it('C339381: 46 iDEAL Checkouting [Payments API]', () => {
-      cy.visit('/SHOP2/en/index.php?controller=history')
+      cy.visit('/en/index.php?controller=history')
       cy.contains('Reorder').click()
       //Billing country LT, DE etc.
       cy.get('.clearfix > .btn').click()
@@ -758,7 +741,7 @@ it('C339382: 47 iDEAL Order BO Refunding, Partial Refunding [Payments API]', () 
       cy.OrderRefundingPartialPaymentsAPI()
 })
 it('C339383: 48 Credit Card Checkouting [Payments API]', () => {
-      cy.visit('/SHOP2/en/index.php?controller=history')
+      cy.visit('/en/index.php?controller=history')
       cy.contains('Reorder').click()
       //Billing country LT, DE etc.
       cy.get('.clearfix > .btn').click()
@@ -792,7 +775,7 @@ it('C339384: 49 Credit Card Order BO Refunding, Partial Refunding [Payments API]
 it.skip('C339385: 50 Credit Card Guest Checkouting [Payments API]', () => {
       cy.clearCookies()
       //Payments API item
-      cy.visit('/SHOP2/en/', { headers: {"Accept-Encoding": "gzip, deflate"}})
+      cy.visit('/en/', { headers: {"Accept-Encoding": "gzip, deflate"}})
       cy.get('[class="h3 product-title"]').eq(0).click()
       cy.get('.add > .btn').click()
       cy.get('.cart-content-btn > .btn-primary').click()
@@ -844,7 +827,7 @@ it.skip('C339385: 50 Credit Card Guest Checkouting [Payments API]', () => {
 it.skip('C339386: 51 Credit Card Guest Checkouting with not 3DS secure card [Payments API]', () => {
       cy.clearCookies()
       //Payments API item
-      cy.visit('/SHOP2/en/', { headers: {"Accept-Encoding": "gzip, deflate"}})
+      cy.visit('/en/', { headers: {"Accept-Encoding": "gzip, deflate"}})
       cy.get('[class="h3 product-title"]').eq(0).click()
       cy.get('.add > .btn').click()
       cy.get('.cart-content-btn > .btn-primary').click()
@@ -878,7 +861,7 @@ it.skip('C339386: 51 Credit Card Guest Checkouting with not 3DS secure card [Pay
       cy.get('#content-hook_order_confirmation > .card-block').should('be.visible')
 })
 it('C339387: 52 Paypal Checkouting [Payments API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       //
       cy.contains('Reorder').click()
       cy.contains('DE').click()
@@ -916,7 +899,7 @@ it('C339388: 53 Paypal BO Refunding, Partial Refunding [Payments API]', () => {
       cy.get('#mollie_order > :nth-child(1) > .alert').contains('Refund was made successfully!')
 });
 it('C339389: 54 SOFORT Checkouting [Payments API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       //
       cy.contains('Reorder').click()
       cy.contains('DE').click()
@@ -950,7 +933,7 @@ it('C339390: 55 SOFORT BO Refunding, Partial Refunding [Payments API]', () => {
       //Refunding is unavailable - information from Mollie Dashboard - but checking the UI itself
 });
 it('C339391: 56 Przelewy24 Checkouting [Payments API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       //
       cy.contains('Reorder').click()
       cy.contains('DE').click()
@@ -983,7 +966,7 @@ it('C339392: 57 Przelewy24 BO Refunding, Partial Refunding [Payments API]', () =
       cy.OrderRefundingPartialPaymentsAPI()
 });
 it('C339393: 58 Giropay Checkouting [Payments API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       //
       cy.contains('Reorder').click()
       cy.contains('DE').click()
@@ -1014,7 +997,7 @@ it('C339394: 59 Giropay BO Refunding, Partial Refunding [Payments API]', () => {
       cy.OrderRefundingPartialPaymentsAPI()
 });
 it('C339395: 60 EPS Checkouting [Payments API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       //
       cy.contains('Reorder').click()
       cy.contains('DE').click()
@@ -1045,7 +1028,7 @@ it('C339396: 61 EPS BO Refunding, Partial Refunding [Payments API]', () => {
       cy.OrderRefundingPartialPaymentsAPI()
 });
 it('C339397: 62 KBC/CBC Checkouting [Payments API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       //
       cy.contains('Reorder').click()
       cy.contains('DE').click()
@@ -1077,7 +1060,7 @@ it('C339398: 63 KBC/CBC BO Refunding, Partial Refunding [Payments API]', () => {
       cy.OrderRefundingPartialPaymentsAPI()
 });
 it('C339399: 64 Belfius Checkouting [Payments API]', () => {
-      cy.visit('/SHOP2/de/index.php?controller=history')
+      cy.visit('/de/index.php?controller=history')
       //
       cy.contains('Reorder').click()
       cy.contains('DE').click()
@@ -1108,7 +1091,7 @@ it('C339400: 65 Belfius BO Refunding, Partial Refunding [Payments API]', () => {
       cy.OrderRefundingPartialPaymentsAPI()
 });
 it('C339401: 66 Bank Transfer Checkouting [Payments API]', () => {
-      cy.visit('/SHOP2/en/index.php?controller=history')
+      cy.visit('/en/index.php?controller=history')
       //
       cy.contains('Reorder').click()
       cy.contains('DE').click()
