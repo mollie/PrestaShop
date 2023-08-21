86363373096fcaaf5fc9538546ebad3392aeb7e9
SimonasB88 <simonas.burneikis@invertus.eu>
2023-08-21 19:21:21 +0300

    ps1784 test spec updates

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   chromeWebSecurity: false,
   experimentalMemoryManagement: true,
   experimentalSourceRewriting: true,
-  numTestsKeptInMemory: 0,
+  numTestsKeptInMemory: 5,
   defaultCommandTimeout: 15000,
   projectId: 'xb89dr',
   retries: 3,
