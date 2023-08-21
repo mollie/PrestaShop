40a53b485b41460da6abbda3aa098d211df8b986
SimonasB88 <simonas.burneikis@invertus.eu>
2023-08-21 11:01:28 +0300

    useful cypress updates

--- cypress/e2e/ps1784/01_mollie.ps1784.ModuleConfiguration.specs.js
+++ cypress/e2e/ps1784/01_mollie.ps1784.ModuleConfiguration.specs.js
@@ -31,24 +31,8 @@ function prepareCookie()
 
         });
       }
-      //Caching the BO and FO session
-      const login = (MollieBOFOLoggingIn) => {
-      cy.session(MollieBOFOLoggingIn,() => {
-      cy.visit('/admin1/')
-      cy.url().should('contain', 'https').as('Check if HTTPS exists')
-      cy.get('#email').type('demo@demo.com',{delay: 0, log: false})
-      cy.get('#passwd').type('demodemo',{delay: 0, log: false})
-      cy.get('#submit_login').click().wait(1000).as('Connection successsful')
-      //switching the multistore PS1784
-      cy.get('#header_shop > .dropdown').click()
-      cy.get('.open > .dropdown-menu').find('[class="shop"]').eq(1).find('[href]').eq(0).click()
-      cy.visit('/SHOP2/index.php?controller=my-account')
-      cy.get('#login-form [name="email"]').eq(0).type('demo@demo.com')
-      cy.get('#login-form [name="password"]').eq(0).type('demodemo')
-      cy.get('#login-form [type="submit"]').eq(0).click({force:true})
-      cy.get('#history-link > .link-item').click()
-      })
-      }
+
+cy.CachingBOFOPS1784()
 
 //Checking the console for errors
 let windowConsoleError;
