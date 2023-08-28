3990f5b8622b10d31811c75c57df6281afe5fcb7
SimonasB88 <simonas.burneikis@invertus.eu>
2023-08-28 11:29:40 +0300

    spec update

--- cypress/e2e/ps1784/04_mollie.ps1784.Subscriptions.js
+++ cypress/e2e/ps1784/04_mollie.ps1784.Subscriptions.js
@@ -44,15 +44,29 @@ it('C176305 Check if Subscription options added in Product BO', () => {
   cy.get('[class="attribute-quantity"]').last().find('[type="text"]').clear().type('999')
   cy.get('#submit').click()
   cy.get('.growl-message').contains('Settings updated.')
-  //Check if Subscription options are in Product Page FO
+  //Check if Subscription options are in Product Page FO and then register the Subscription product by purchasing it
   cy.visit('/de/')
-  cy.get('[data-id-product="8"]').click() //possible PS1784 notice exception, checking with dev...
-  //wip ...
+  cy.get('[data-id-product="8"]').click()
+  cy.get('[aria-label="Subscription"]').should('be.visible') //asserting if there is a Subscription dropdown in product page
+  cy.contains('Add to cart').click()
+  cy.contains('Proceed to checkout').click()
+  cy.contains('Proceed to checkout').click()
+  cy.contains('DE').click()
+  cy.get('.clearfix > .btn').click()
+  cy.get('#js-delivery > .continue').click()
+  //Payment method choosing
+  cy.contains('Karte').click({force:true})
+  //Credit card inputing
+  cy.CreditCardFillingIframe()
+  cy.get('.condition-label > .js-terms').click({force:true})
+  cy.get('.ps-shown-by-js > .btn').click()
+  cy.get('[value="paid"]').click()
+  cy.get('[class="button form__button"]').click()
+  cy.get('#content-hook_order_confirmation > .card-block').should('be.visible')
   //Check if Subscription options are implemented in My Account FO
   cy.visit('/en/')
   cy.get('[class="account"]').click()
   cy.contains('Subscriptions').click()
   cy.get('[class="page-content"]').should('be.visible')
-  //wip ...
 });
 })
