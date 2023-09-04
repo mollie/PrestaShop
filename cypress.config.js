be2c8d2513534ac70b45368f3c13085d5046c87d
SimonasB88 <simonas.burneikis@invertus.eu>
2023-09-04 10:29:54 +0300

    cypress updates

--- cypress.config.js
+++ cypress.config.js
@@ -16,6 +16,18 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      on('after:spec', (spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) =>
+            test.attempts.some((attempt) => attempt.state === 'failed')
+          )
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            fs.unlinkSync(results.video)
+          }
+        }
+      })
       require('./cypress/plugins/index.js')(on, config)
       require("cypress-fail-fast/plugin")(on, config);
       require('cypress-terminal-report/src/installLogsPrinter')(on);
