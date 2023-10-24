8fd6928e07692e7b3f2a063273739243d66727d2
mandan2 <61560082+mandan2@users.noreply.github.com>
2023-10-24 09:39:02 +0300

    PIPRES-348: Move subscription options to subscription tab (#832)
    * PIPRES-348: WIP move subscription options to subscription tab
    
    * added carrier repository to provide with carrier choices, moved template to another file
    
    * phpstan
    
    * adjusted PS 1.7.6 support
    
    * removed validations from formProcess
    
    * simplified formProcess

--- subscription/Controller/Symfony/AbstractSymfonyController.php
+++ subscription/Controller/Symfony/AbstractSymfonyController.php
@@ -4,7 +4,6 @@ namespace Mollie\Subscription\Controller\Symfony;
 
 use Module;
 use Mollie;
-use Mollie\ServiceProvider\LeagueServiceContainerProvider;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 
 /**
@@ -12,16 +11,13 @@ use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
  */
 abstract class AbstractSymfonyController extends FrameworkBundleAdminController
 {
-    /** @var LeagueServiceContainerProvider */
-    protected $leagueContainer;
-
     /** @var Mollie */
     protected $module;
 
     public function __construct()
     {
         parent::__construct();
-        $this->leagueContainer = new LeagueServiceContainerProvider();
+
         /* @phpstan-ignore-next-line */
         $this->module = Module::getInstanceByName('mollie');
     }
---
+++ subscription/Form/Options/SubscriptionOptionsType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Mollie\Subscription\Form\Options;
+
+use Module;
+use PrestaShop\PrestaShop\Core\Form\FormChoiceProviderInterface;
+use PrestaShopBundle\Form\Admin\Type\TranslatorAwareType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class SubscriptionOptionsType extends TranslatorAwareType
+{
+    /** @var FormChoiceProviderInterface */
+    private $carrierOptionProvider;
+    /** @var Module */
+    private $module;
+
+    public function __construct(
+        TranslatorInterface $translator,
+        array $locales,
+        FormChoiceProviderInterface $carrierOptionProvider,
+        Module $module
+    ) {
+        parent::__construct($translator, $locales);
+
+        $this->carrierOptionProvider = $carrierOptionProvider;
+        $this->module = $module;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('carrier', ChoiceType::class, [
+                'required' => true,
+                'choices' => $this->carrierOptionProvider->getChoices(),
+                // TODO migrate to modern translation system
+                'placeholder' => $this->module->l('Choose your carrier'),
+            ]);
+    }
+}
