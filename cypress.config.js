f10ab9998862309a1db60f4e6dac60b3f536492b
SimonasB88 <simonas.burneikis@invertus.eu>
2023-11-06 13:28:16 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   experimentalMemoryManagement: true,
   experimentalSourceRewriting: true,
   numTestsKeptInMemory: 0,
-  defaultCommandTimeout: 80000,
+  defaultCommandTimeout: 8000,
   projectId: 'xb89dr',
   retries: 1,
   videoCompression: 8,
