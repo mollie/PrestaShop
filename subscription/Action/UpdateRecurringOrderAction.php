89db2d35eef6a7b75b91f09080afe6ea22649b89
mandan2 <61560082+mandan2@users.noreply.github.com>
2023-12-11 11:42:37 +0200

    PIPRES-349: Update recurring order action (#860)
    * PIPRES-349: Subscription carrier data provider
    
    * renamed service and added missing DTO
    
    * fixed return type
    
    * renamed service
    
    * PIPRES-349: Update recurring order action
    
    * csfixer

---
+++ subscription/Action/UpdateRecurringOrderAction.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Mollie       https://www.mollie.nl
+ *
+ * @author      Mollie B.V. <info@mollie.nl>
+ * @copyright   Mollie B.V.
+ * @license     https://github.com/mollie/PrestaShop/blob/master/LICENSE.md
+ *
+ * @see        https://github.com/mollie/PrestaShop
+ * @codingStandardsIgnoreStart
+ */
+
+namespace Mollie\Subscription\Action;
+
+use Mollie\Logger\PrestaLoggerInterface;
+use Mollie\Subscription\DTO\UpdateRecurringOrderData;
+use Mollie\Subscription\Exception\CouldNotUpdateRecurringOrder;
+use Mollie\Subscription\Exception\MollieSubscriptionException;
+use Mollie\Subscription\Utility\ClockInterface;
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class UpdateRecurringOrderAction
+{
+    /** @var PrestaLoggerInterface */
+    private $logger;
+    /** @var ClockInterface */
+    private $clock;
+
+    public function __construct(
+        PrestaLoggerInterface $logger,
+        ClockInterface $clock
+    ) {
+        $this->logger = $logger;
+        $this->clock = $clock;
+    }
+
+    /**
+     * @throws MollieSubscriptionException
+     */
+    public function run(UpdateRecurringOrderData $data): \MolRecurringOrder
+    {
+        $this->logger->debug(sprintf('%s - Function called', __METHOD__));
+
+        try {
+            $recurringOrder = new \MolRecurringOrder($data->getMollieRecurringOrderId());
+
+            /*
+             * NOTE: When more properties will be needed to update, pass them up as nullable parameters.
+             */
+            $recurringOrder->total_tax_incl = $data->getSubscriptionTotalAmount();
+            $recurringOrder->date_update = $this->clock->getCurrentDate();
+
+            $recurringOrder->add();
+        } catch (\Throwable $exception) {
+            throw CouldNotUpdateRecurringOrder::unknownError($exception);
+        }
+
+        $this->logger->debug(sprintf('%s - Function ended', __METHOD__));
+
+        return $recurringOrder;
+    }
+}
