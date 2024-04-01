3a7aab76c3bc5c79de687f80890704db5ad03a88
jevgenijvisockij <jevgenij.visockij@gmail.com>
2024-04-01 10:35:18 +0300

    Translations to Mollie module
    This commit introduces Spanish and Italian translations for the Mollie module in PrestaShop.

---
+++ translations/it.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Mollie       https://www.mollie.nl
+ *
+ * @author      Mollie B.V. <info@mollie.nl>
+ * @copyright   Mollie B.V.
+ * @license     https://github.com/mollie/PrestaShop/blob/master/LICENSE.md
+ *
+ * @see        https://github.com/mollie/PrestaShop
+ * @codingStandardsIgnoreStart
+ */
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{mollie}prestashop>mollie_iframe_a4b29064de828b87f9f9b6fa145cb507'] = 'Inserisci le informazioni della tua carta';
+$_MODULE['<{mollie}prestashop>mollie_iframe_c2b63e85bd5e4dc9b6cf5a4693847e06'] = 'Intestatario della carta';
+$_MODULE['<{mollie}prestashop>mollie_iframe_a44217022190f5734b2f72ba1e4f8a79'] = 'Numero della carta';
+$_MODULE['<{mollie}prestashop>mollie_iframe_95b16127e70e8a90220404fb48343182'] = 'Data di scadenza';
+$_MODULE['<{mollie}prestashop>mollie_iframe_b2fddfad59392b3dba82cb9809712197'] = 'CVC/CVV';
+$_MODULE['<{mollie}prestashop>mollie_iframe_fc7775bdfa339217149389b7392af83b'] = 'Salva la carta';
+$_MODULE['<{mollie}prestashop>mollie_iframe_ee2b8214cd8479d18e873181505b48ea'] = 'Usa la carta salvata';
+$_MODULE['<{mollie}prestashop>mollie_iframe_49753ba0559e716a708373951ea87d1c'] = 'Pagamenti sicuri forniti da';
