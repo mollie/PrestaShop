7780554bbecfc0ec19f7d71f12941cd7477224aa
SimonasB88 <simonas.burneikis@invertus.eu>
2024-04-03 11:57:38 +0300

    fixing payment namings

--- cypress/e2e/ps8/03_mollie.ps8.PaymentTestsOrdersAPI.js
+++ cypress/e2e/ps8/03_mollie.ps8.PaymentTestsOrdersAPI.js
@@ -128,32 +128,32 @@ it('C339355: 18 Check if customerId is passed during the 2nd payment using Singl
 it('C339356: 19 Credit Card Order BO Shipping, Refunding [Orders API]', () => {
       cy.OrderRefundingShippingOrdersAPI()
 })
-it('C339357: 20 IN3 Checkouting [Orders API]', () => { // wip
+it.only('C339357: 20 IN3 Checkouting [Orders API]', () => { // wip
       cy.navigatingToThePaymentPS8()
       //Payment method choosing
       // waiting for enabling IN3 payment
-      cy.contains('in3').click({force:true})
+      cy.contains('in 3').click({force:true})
       cy.get('.condition-label > .js-terms').click({force:true})
       cy.contains('Place order').click()
       cy.get('[value="paid"]').click()
       cy.get('[class="button form__button"]').click()
       cy.get('#content-hook_order_confirmation > .card-block').should('be.visible')
 })
-it('C339358: 21 IN3 Order BO Shipping, Refunding [Orders API]', () => { // checking why payment div is not loaded in the Orders for some reason
+it('C339358: 21 IN3 Order BO Shipping, Refunding [Orders API]', () => {
       cy.OrderRefundingShippingOrdersAPI()
 })
 it('C339359: 22 IN3 should not be shown under 5000 EUR [Orders API]', () => {
       cy.visit('/en/index.php?controller=history')
       cy.contains('Reorder').click()
       cy.visit('/en/cart?action=show')
-      cy.get('[class="js-cart-line-product-quantity form-control"]').eq(0).clear().type('250')
+      cy.get('[class="js-cart-line-product-quantity form-control"]').eq(0).clear().type('200')
       cy.contains('Proceed to checkout').click()
       cy.contains('NL').click()
       //Billing country LT, DE etc.
       cy.get('.clearfix > .btn').click()
       cy.get('#js-delivery > .continue').click()
       //Payment method choosing
-      cy.contains('in3').should('not.exist')
+      cy.contains('in 3').should('not.exist')
 })
 it('C339360: 23 IN3 Checking that IN3 logo exists OK [Orders API]', () => {
       cy.visit('/admin1/')
