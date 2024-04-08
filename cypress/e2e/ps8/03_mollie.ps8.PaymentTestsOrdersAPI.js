1566cec380d7287b4bbe5c9427496ef277690f17
SimonasB88 <simonas.burneikis@invertus.eu>
2024-04-08 19:42:24 +0300

    adding iPhone X viewport E2E test

--- cypress/e2e/ps8/03_mollie.ps8.PaymentTestsOrdersAPI.js
+++ cypress/e2e/ps8/03_mollie.ps8.PaymentTestsOrdersAPI.js
@@ -307,7 +307,7 @@ it('C1765085: Billie Checkouting [Orders API]', () => {
 it('C1765086: Billie Order Shipping, Refunding [Orders API]', () => {
       cy.OrderShippingRefundingOrdersAPI()
 })
-it('C1860460: Pay with Klarna UK Checkouting [Orders API]', () => { // currently not supported for PS, skipping temporary
+it('C1860460: Pay with Klarna UK Checkouting [Orders API]', () => {
   cy.visit('/en/order-history')
   cy.contains('Reorder').click()
   cy.contains('UK').click({force:true})
@@ -322,7 +322,6 @@ it('C1860460: Pay with Klarna UK Checkouting [Orders API]', () => { // currently
   cy.get('[class="button form__button"]').click()
   cy.get('#content-hook_order_confirmation > .card-block').should('be.visible')
 });
-it('C1860461: Pay with Klarna UK Order Shipping, Refunding [Orders API]', () => { // currently not supported for PS, skipping temporary
-  cy.OrderShippingRefundingOrdersAPI()
+it('C1860461: Pay with Klarna UK Order Shipping, Refunding [Orders API]', () => {
 })
 })
