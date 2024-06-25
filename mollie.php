b119af559f22d080206cd6ed0ece39c16986f588
Gytautas Zumaras <96050852+GytisZum@users.noreply.github.com>
2024-06-25 16:29:17 +0300

    Update mollie.php

--- mollie.php
+++ mollie.php
@@ -84,7 +84,7 @@ class Mollie extends PaymentModule
     {
         $this->name = 'mollie';
         $this->tab = 'payments_gateways';
-        $this->version = '6.2.0';
+        $this->version = '6.2.1';
         $this->author = 'Mollie B.V.';
         $this->need_instance = 1;
         $this->bootstrap = true;
