2501effe6b8fe97fc9dfc02322ffece51d36e34a
Justas Vaitkus <j.vaitkus@Justass-MacBook-Air.local>
2024-11-15 20:39:38 +0200

    fix

--- cypress/e2e/ps8/05_mollie.ps8.PaymentTestsPaymentsAPI.js
+++ cypress/e2e/ps8/05_mollie.ps8.PaymentTestsPaymentsAPI.js
@@ -161,7 +161,6 @@ it('C339391: 56 Przelewy24 Checkouting [Payments API]', () => {
     cy.contains('Przelewy24').click({force:true})
     cy.get('.condition-label > .js-terms').click({force:true})
     cy.contains('Place order').click()
-    cy.get('.input-float > input').type('testing@testing.com')
     cy.get('[value="paid"]').click()
     cy.get('[class="button form__button"]').click()
     cy.get('#content-hook_order_confirmation > .card-block').should('be.visible')
