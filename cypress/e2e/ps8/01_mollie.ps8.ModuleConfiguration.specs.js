7ae16b94bd3f4bcf28fe439c5a5bdb730fb596e3
Justas Vaitkus <j.vaitkus@Justass-MacBook-Air.local>
2024-11-19 15:23:42 +0200

    removing cloudsync validation as it causes delay

--- cypress/e2e/ps8/01_mollie.ps8.ModuleConfiguration.specs.js
+++ cypress/e2e/ps8/01_mollie.ps8.ModuleConfiguration.specs.js
@@ -27,8 +27,8 @@ it('C339305: Connecting test API successsfully', () => {
       .then((text) => {
       cy.log(text) // Showing and asserting the text that loaded, to ensure the BO is loaded, not crashed with PHP fatals etc.
       })
-      cy.iframe('[id^="uid_"]').find('button').click() // Cloudsync validation
-      cy.wait(15000) // Waiting for validation to process until the end
+      // cy.iframe('[id^="uid_"]').find('button').click() // Cloudsync validation
+      // cy.wait(15000) // Waiting for validation to process until the end
       cy.get('#MOLLIE_ACCOUNT_SWITCH_on').click({force:true})
       cy.get('#MOLLIE_API_KEY_TEST').type((Cypress.env('MOLLIE_TEST_API_KEY')),{delay: 0, log: false})
       cy.get('#module_form_submit_btn').click()
