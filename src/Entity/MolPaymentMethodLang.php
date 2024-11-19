3d695be314c7651cc3afa41a1388096ce193d9fc
MarijusCoding <marijusdilys9@gmail.com>
2024-11-19 17:23:44 +0200

    [PIPRES-468] object model

---
+++ src/Entity/MolPaymentMethodLang.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Mollie       https://www.mollie.nl
+ *
+ * @author      Mollie B.V. <info@mollie.nl>
+ * @copyright   Mollie B.V.
+ * @license     https://github.com/mollie/PrestaShop/blob/master/LICENSE.md
+ *
+ * @see        https://github.com/mollie/PrestaShop
+ * @codingStandardsIgnoreStart
+ */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class MolPaymentMethodLang extends ObjectModel
+{
+    /** @var int */
+    public $id;
+
+    /** @var int name of the payment method */
+    public $id_method;
+
+    /** @var int */
+    public $id_lang;
+
+    /** @var int */
+    public $id_shop;
+
+    /** @var string payment title */
+    public $text;
+
+    /**
+     * Definition of the ObjectModel
+     */
+    public static $definition = [
+        'table' => 'mol_payment_method_lang',
+        'primary' => 'id',
+        'fields' => [
+            'id_method' => [
+                'type' => self::TYPE_INT,
+                'validate' => 'isUnsignedInt',
+                'required' => true,
+            ],
+            'id_lang' => [
+                'type' => self::TYPE_INT,
+                'validate' => 'isUnsignedInt',
+                'required' => true,
+            ],
+            'id_shop' => [
+                'type' => self::TYPE_INT,
+                'validate' => 'isUnsignedInt',
+                'required' => true,
+            ],
+            'text' => [
+                'type' => self::TYPE_STRING,
+                'validate' => 'isString',
+                'size' => 255,
+            ],
+        ],
+    ];
+}
