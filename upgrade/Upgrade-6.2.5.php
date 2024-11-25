056b7f61dd1cb3b11a4a01adf0bb86c386bbfd6e
MarijusCoding <marijusdilys9@gmail.com>
2024-11-25 14:24:52 +0200

    [PIPRES-468] updated upgrade logic to add all titles into new table on upgrade

--- upgrade/Upgrade-6.2.5.php
+++ upgrade/Upgrade-6.2.5.php
@@ -15,7 +15,6 @@ if (!defined('_PS_VERSION_')) {
 
 function upgrade_module_6_2_5(Mollie $module): bool
 {
-    // Create the new mol_payment_method_lang table
     $sql = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'mol_payment_method_lang` (
                 `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
                 `id_method` TINYTEXT,
@@ -23,7 +22,46 @@ function upgrade_module_6_2_5(Mollie $module): bool
                 `id_shop` INT(11),
                 `text` VARCHAR(64) NOT NULL,
                 INDEX (`id_method`)
-    ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;';
+        ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;';
+
+    $result = Db::getInstance()->execute($sql);
+
+    if(!$result) {
+        return false;
+    }
+
+    try {
+        deleteAndUpdatePaymentMethodTitles();
+    } catch (Exception $e) {
+        return false;
+    }
+
+    $sql = 'ALTER TABLE `mol_payment_method` DROP COLUMN `title`;';
 
     return Db::getInstance()->execute($sql);
+}
+
+function deleteAndUpdatePaymentMethodTitles() {
+    $sql = 'SELECT `id_method`, `title` FROM `' . _DB_PREFIX_ . 'mol_payment_method`';
+
+    $methodsList = \Db::getInstance()->executeS($sql);
+
+    foreach ($methodsList as $method) {
+        if (empty($method['title'])) {
+            continue;
+        }
+
+        insertNewTitlesIntoDatabase($method);
+    }
+}
+
+function insertNewTitlesIntoDatabase($method) {
+    foreach (\Shop::getCompleteListOfShopsID() as $idShop) {
+        foreach (\Language::getLanguages() as $idLang) {
+            $sql = 'INSERT INTO `' . _DB_PREFIX_ . 'mol_payment_method_lang` (`id_method`, `id_lang`, `id_shop`, `text`)
+                    VALUES ("' . pSQL($method['id_method']) . '", ' . (int)$idLang['id_lang'] . ', ' . (int)$idShop . ', "' . pSQL($method['title']) . '")';
+
+            \Db::getInstance()->execute($sql);
+        }
+    }
 }
