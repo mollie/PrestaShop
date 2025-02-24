ac56aa330fc29273a14a0d9efda2a6bff038987c
MarijusCoding <marijusdilys9@gmail.com>
2025-02-24 12:23:50 +0200

    [PIPRES-545] fix

--- upgrade/Upgrade-6.2.7.php
+++ upgrade/Upgrade-6.2.7.php
@@ -17,7 +17,9 @@ function upgrade_module_6_2_7(Mollie $module): bool
 {
     $sql = 'ALTER TABLE `' . _DB_PREFIX_ . 'mol_payment_method_lang` RENAME TO ' . _DB_PREFIX_ . 'mol_payment_method_translations;';
 
+    $result = Db::getInstance()->execute($sql);
+
     Tools::clearAllCache();
 
-    return Db::getInstance()->execute($sql);
+    return $result;
 }
