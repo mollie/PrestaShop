35703ebf4812da58a35ef21477ed633c35d88130
MarijusCoding <marijusdilys9@gmail.com>
2025-03-04 12:00:09 +0200

    [PIPRES-460] tests

---
+++ tests/Unit/Service/CartLine/CartItemShippingLineServiceTest.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Mollie       https://www.mollie.nl
+ *
+ * @author      Mollie B.V. <info@mollie.nl>
+ * @copyright   Mollie B.V.
+ * @license     https://github.com/mollie/PrestaShop/blob/master/LICENSE.md
+ *
+ * @see        https://github.com/mollie/PrestaShop
+ * @codingStandardsIgnoreStart
+ */
+
+namespace Mollie\Service\CartLine;
+
+use Mollie\Tests\Unit\BaseTestCase;
+
+class CartItemShippingLineServiceTest extends BaseTestCase
+{
+    /** @dataProvider dataProvider */
+    public function testItAddsDiscount($totalDiscount, $orderLines, $expected)
+    {
+        $cartItemShippingLineService = new CartItemShippingLineService($this->languageService);
+
+        $cart = $this->getMockBuilder(\Cart::class) ->setMethods(['getSummaryDetails'])->getMock();
+
+        $cart->method('getSummaryDetails')->willReturn([
+            'total_shipping' => 10,
+            'total_shipping_tax_exc' => 10
+        ]);
+
+        $result = $cartItemShippingLineService->addShippingLine(10, $cart->getSummaryDetails(), $orderLines);
+        print_r($result);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function dataProvider()
+    {
+        return [
+            'case1' => [
+                'totalDiscount' => 10,
+                'orderLines' => [
+                    'orderLines' => [
+                        [
+                            'type' => 'product',
+                            'name' => 'product_1',
+                            'quantity' => 1,
+                            'unitPrice' => 10,
+                            'totalAmount' => 10,
+                            'vatRate' => 0,
+                            'vatAmount' => 0,
+                        ],
+                    ],
+                ],
+                'expected' => [
+                    'orderLines' => [
+                        [
+                            'type' => 'product',
+                            'name' => 'product_1',
+                            'quantity' => 1,
+                            'unitPrice' => 10,
+                            'totalAmount' => 10,
+                            'vatRate' => 0,
+                            'vatAmount' => 0,
+                        ],
+                    ],
+                    'shipping' => [
+                        0 => [
+                            'name' => null,
+                            'quantity' => 1,
+                            'unitPrice' => 10.0,
+                            'totalAmount' => 10.0,
+                            'vatRate' => 0.0,
+                            'vatAmount' => 0.0
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+}
