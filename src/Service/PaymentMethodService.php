9503f1aa068124b23a8cfb11a301213e627abf08
MarijusCoding <marijusdilys9@gmail.com>
2025-04-28 12:25:03 +0300

    [PIPRES-579] remove useless code

--- src/Service/PaymentMethodService.php
+++ src/Service/PaymentMethodService.php
@@ -184,18 +184,8 @@ class PaymentMethodService
         $paymentMethod->images_json = json_encode($method['image']);
         $paymentMethod->live_environment = $environment;
         $paymentMethod->id_shop = $shopId;
-
-        $minAmount = (float) Tools::getValue(Mollie\Config\Config::MOLLIE_METHOD_MIN_AMOUNT . $method['id']);
-        $maxAmount = (float) Tools::getValue(Mollie\Config\Config::MOLLIE_METHOD_MAX_AMOUNT . $method['id']);
-
-        $paymentMethod->min_amount = $minAmount;
-
-//        if ($maxAmount < $minAmount) {
-//            throw new MollieException('The maximum amount cannot be lower than the minimum amount.');
-//        }
-
-        $paymentMethod->max_amount = $maxAmount;
-
+        $paymentMethod->min_amount = (float) Tools::getValue(Mollie\Config\Config::MOLLIE_METHOD_MIN_AMOUNT . $method['id']);
+        $paymentMethod->max_amount = (float) Tools::getValue(Mollie\Config\Config::MOLLIE_METHOD_MAX_AMOUNT . $method['id']);
         $paymentMethod->save();
 
         foreach (Tools::getValue(Config::MOLLIE_METHOD_TITLE . $method['id']) as $idLang => $title) {
