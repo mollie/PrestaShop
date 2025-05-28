2032f034067f3391d42ab3611e439978c3fa8515
Marijus Dilys <marijusdilys9@gmail.com>
2025-05-28 12:48:55 +0300

    [PIPRES-594] initialize fix

--- src/ServiceProvider/BaseServiceProvider.php
+++ src/ServiceProvider/BaseServiceProvider.php
@@ -19,7 +19,6 @@ use Mollie;
 use Mollie\Adapter\ConfigurationAdapter;
 use Mollie\Adapter\Context;
 use Mollie\Adapter\Shop;
-use Mollie\Adapter\ToolsAdapter;
 use Mollie\Builder\ApiTestFeedbackBuilder;
 use Mollie\Factory\ModuleFactory;
 use Mollie\Handler\Api\OrderEndpointPaymentTypeHandler;
@@ -32,8 +31,6 @@ use Mollie\Handler\PaymentOption\PaymentOptionHandler;
 use Mollie\Handler\PaymentOption\PaymentOptionHandlerInterface;
 use Mollie\Handler\RetryHandler;
 use Mollie\Handler\RetryHandlerInterface;
-use Mollie\Handler\Settings\CustomerGroupRestrictionHandler;
-use Mollie\Handler\Settings\CustomerGroupRestrictionHandlerInterface;
 use Mollie\Handler\Settings\PaymentMethodPositionHandler;
 use Mollie\Handler\Settings\PaymentMethodPositionHandlerInterface;
 use Mollie\Handler\Shipment\ShipmentSenderHandler;
@@ -117,7 +114,6 @@ use Mollie\Service\PaymentMethod\PaymentMethodRestrictionValidation\AmountPaymen
 use Mollie\Service\PaymentMethod\PaymentMethodRestrictionValidation\ApplePayPaymentMethodRestrictionValidator;
 use Mollie\Service\PaymentMethod\PaymentMethodRestrictionValidation\B2bPaymentMethodRestrictionValidator;
 use Mollie\Service\PaymentMethod\PaymentMethodRestrictionValidation\BasePaymentMethodRestrictionValidator;
-use Mollie\Service\PaymentMethod\PaymentMethodRestrictionValidation\CustomerGroupPaymentMethodRestrictionValidator;
 use Mollie\Service\PaymentMethod\PaymentMethodRestrictionValidation\EnvironmentVersionSpecificPaymentMethodRestrictionValidator;
 use Mollie\Service\PaymentMethod\PaymentMethodRestrictionValidation\VoucherPaymentMethodRestrictionValidator;
 use Mollie\Service\PaymentMethod\PaymentMethodRestrictionValidationInterface;
@@ -262,7 +258,6 @@ final class BaseServiceProvider
                 $container->get(ApplePayPaymentMethodRestrictionValidator::class),
                 $container->get(AmountPaymentMethodRestrictionValidator::class),
                 $container->get(B2bPaymentMethodRestrictionValidator::class),
-                $container->get(CustomerGroupPaymentMethodRestrictionValidator::class),
             ]);
         });
 
@@ -304,12 +299,11 @@ final class BaseServiceProvider
         $this->addServiceArgument($service, Shop::class);
         $this->addServiceArgument($service, TaxCalculatorProvider::class);
         $this->addServiceArgument($service, Context::class);
+        $this->addServiceArgument($service, PaymentMethodLangRepositoryInterface::class);
+        $this->addServiceArgument($service, CustomerRepository::class);
 
         $service = $this->addService($container, EntityManagerInterface::class, ObjectModelEntityManager::class);
         $this->addServiceArgument($service, ObjectModelUnitOfWork::class);
-
-        $service = $this->addService($container, CustomerGroupRestrictionHandlerInterface::class, CustomerGroupRestrictionHandler::class);
-        $this->addServiceArgument($service, ToolsAdapter::class);
     }
 
     private function addService(Container $container, $className, $service)
