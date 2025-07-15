9d44df6a4b24e14550be175be6f35bdd6123d864
MarijusDilys <marijus.dilys@invertus.eu>
2025-07-15 10:46:22 +0300

    [INTERNAL] fix linter

--- tests/Unit/Presentation/Admin/AdminModuleTabTranslatorTest.php
+++ tests/Unit/Presentation/Admin/AdminModuleTabTranslatorTest.php
@@ -1,5 +1,14 @@
 <?php
-declare(strict_types=1);
+/**
+ * Mollie       https://www.mollie.nl
+ *
+ * @author      Mollie B.V. <info@mollie.nl>
+ * @copyright   Mollie B.V.
+ * @license     https://github.com/mollie/PrestaShop/blob/master/LICENSE.md
+ *
+ * @see        https://github.com/mollie/PrestaShop
+ * @codingStandardsIgnoreStart
+ */
 
 namespace Mollie\Tests\Unit\Presentation\Admin;
 
