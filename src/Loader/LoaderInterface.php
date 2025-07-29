b29f13543db1095004cb6fc98d29c175c8cfcb8d
MarijusDilys <marijus.dilys@invertus.eu>
2025-07-29 11:02:01 +0300

    [PIPRES-601] add licence

--- src/Loader/LoaderInterface.php
+++ src/Loader/LoaderInterface.php
@@ -1,5 +1,15 @@
 
 <?php
+/**
+ * Mollie       https://www.mollie.nl
+ *
+ * @author      Mollie B.V. <info@mollie.nl>
+ * @copyright   Mollie B.V.
+ * @license     https://github.com/mollie/PrestaShop/blob/master/LICENSE.md
+ *
+ * @see        https://github.com/mollie/PrestaShop
+ * @codingStandardsIgnoreStart
+ */
 
 namespace Mollie\Loader;
 
