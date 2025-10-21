72feee72bad2fb3d1cd253f1f64e28560e6cbbf7
MarijusDilys <marijus.dilys@invertus.eu>
2025-10-21 10:43:45 +0300

    init minor fixes

--- controllers/admin/AdminMollieOrderController.php
+++ controllers/admin/AdminMollieOrderController.php
@@ -1,5 +1,16 @@
 <?php
 /**
+ * Mollie       https://www.mollie.nl
+ *
+ * @author      Mollie B.V. <info@mollie.nl>
+ * @copyright   Mollie B.V.
+ * @license     https://github.com/mollie/PrestaShop/blob/master/LICENSE.md
+ *
+ * @see        https://github.com/mollie/PrestaShop
+ * @codingStandardsIgnoreStart
+ */
+
+/**
  * Admin controller for handling Mollie order actions: refund, capture, ship.
  *
  * Follows PrestaShop module conventions and delegates business logic to services.
